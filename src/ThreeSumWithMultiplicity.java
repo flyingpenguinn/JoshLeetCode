fc2d81d1ea7f04fd9843c7233f1faa886db5a9f9
flyingpenguinn <zjm84812@gmail.com>
2020-05-18 21:41:21 -0400

    Update ThreeSumWithMultiplicity.java

--- src/ThreeSumWithMultiplicity.java
+++ src/ThreeSumWithMultiplicity.java
@@ -1,6 +1,41 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+LC#923
+Given an integer array A, and an integer target, return the number of tuples i, j, k  such that i < j < k and A[i] + A[j] + A[k] == target.
+
+As the answer can be very large, return it modulo 10^9 + 7.
 
 
+
+Example 1:
+
+Input: A = [1,1,2,2,3,3,4,4,5,5], target = 8
+Output: 20
+Explanation:
+Enumerating by the values (A[i], A[j], A[k]):
+(1, 2, 5) occurs 8 times;
+(1, 3, 4) occurs 8 times;
+(2, 2, 4) occurs 2 times;
+(2, 3, 3) occurs 2 times.
+Example 2:
+
+Input: A = [1,1,2,2,2,2], target = 5
+Output: 12
+Explanation:
+A[i] = 1, A[j] = A[k] = 2 occurs 12 times:
+We choose one 1 from [1,1] in 2 ways,
+and two 2s from [2,2,2,2] in 6 ways.
+
+
+Note:
+
+3 <= A.length <= 3000
+0 <= A[i] <= 100
+0 <= target <= 300
+ */
 public class ThreeSumWithMultiplicity {
     // O(n+size*size)  in this one possible range of numbers is small
     // idea is to loop numbers themselves
@@ -58,67 +93,69 @@ class ThreeSumWithMultiSol2 {
 
     private int MOD = 1000000007;
 
-    public int threeSumMulti(int[] nums, int target) {
-        Arrays.sort(nums);
-        long count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int ti = target - nums[i];
+    public int threeSumMulti(int[] a, int target) {
+        Arrays.sort(a);
+        long r = 0;
+        for (int i = 0; i < a.length; i++) {
+            int ti = target - a[i];
             int j = i + 1;
-            int k = nums.length - 1;
+            int k = a.length - 1;
             while (j < k) {
-                int sumjk = nums[j] + nums[k];
+                int sumjk = a[j] + a[k];
                 if (sumjk > ti) {
                     k--;
                 } else if (sumjk < ti) {
                     j++;
 
                 } else {
-                    // note this is different from 3sum smaller because when == we really dont know where to go:
-                    // either directly j++ or directly k-- may be bad because of duplciated j or k values
-                    if (nums[j] == nums[k]) {
-                        int diff = k - j + 1;
-                        int delta = diff * (diff - 1) / 2;
-                        count += delta;
+                    // note this is different from 3sum smaller when == this is no longer sizej*sizek
+                    if (a[j] == a[k]) {
+                        int gap = k - j + 1;
+                        int delta = gap * (gap - 1) / 2;
+                        r += delta;
+                        r %= MOD;
                         break;
                     }
-                    int jv = nums[j];
-                    int jcount = 0;
-                    while (j < k && nums[j] == jv) {
+                    int oj = j;
+                    while (a[j] == a[oj]) {
                         j++;
-                        jcount++;
                     }
-                    j--;// back from the last ++ value which could be bad
-                    int kv = nums[k];
-                    int kcount = 0;
+                    int ok = k;
                     // here j can == k because the j earlier is not sth we would land on
-                    while (j < k && nums[k] == kv) {
+                    while (a[k] == a[ok]) {
                         k--;
-                        kcount++;
                     }
-                    int delta = jcount * kcount;
-                    count += delta;
+                    int delta = (j - oj) * (ok - k);
+                    r += delta;
+                    r %= MOD;
                 }
             }
         }
-        return (int) (count % MOD);
+        return (int) r;
     }
 }
 
 class ThreeSumWithMultiSol3 {
-    // fix j, loop i and count how many ks we have
+    // fix j, loop i and count how many ks we have. note in 3 index problem we usually fix middle and loop the two sides
+
+    long MOD = 1000000007;
+
     public int threeSumMulti(int[] a, int t) {
-        int[] cnt = new int[101];
+
+        Arrays.sort(a);
         int n = a.length;
         long r = 0;
-        for (int j = n - 1; j >= 0; j--) {
-            for (int i = 0; i < j; i++) {
-                int tk = t - a[j] - a[i];
-                if (tk < cnt.length && tk >= 0 && cnt[tk] > 0) {
-                    r += cnt[tk];
+        int[] m = new int[101];
+        for (int j = 0; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                int ti = t - a[j] - a[k];
+                if (ti >= 0 && ti <= 100) {
+                    r += m[ti];
+                    r %= MOD;
                 }
             }
-            cnt[a[j]]++;
+            m[a[j]]++;
         }
-        return (int) (r % 1000000007);
+        return (int) r;
     }
 }
