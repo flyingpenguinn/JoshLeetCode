43e82b435dead0cbe7aa89fbf42e60274f8a39d2
flyingpenguinn <zjm84812@gmail.com>
2020-05-25 19:53:04 -0400

    Update BulbSwitcherII.java

--- src/BulbSwitcherII.java
+++ src/BulbSwitcherII.java
@@ -1,20 +1,56 @@
+/*
+LC#672
+There is a room with n lights which are turned on initially and 4 buttons on the wall. After performing exactly m unknown operations towards buttons, you need to return how many different kinds of status of the n lights could be.
+
+Suppose n lights are labeled as number [1, 2, 3 ..., n], function of these 4 buttons are given below:
+
+Flip all the lights.
+Flip lights with even numbers.
+Flip lights with odd numbers.
+Flip lights with (3k + 1) numbers, k = 0, 1, 2, ...
+
+
+Example 1:
+
+Input: n = 1, m = 1.
+Output: 2
+Explanation: Status can be: [on], [off]
+
+
+Example 2:
+
+Input: n = 2, m = 1.
+Output: 3
+Explanation: Status can be: [on, off], [off, on], [off, off]
+
+
+Example 3:
+
+Input: n = 3, m = 1.
+Output: 4
+Explanation: Status can be: [off, on, off], [on, off, on], [off, off, off], [off, on, on].
+
+
+Note: n and m both fit in range [0, 1000].
+ */
 public class BulbSwitcherII {
     /*
-As before, the first 6 lights uniquely determine the rest of the lights. This is because every operation that
-modifies the xx-th light also modifies the (x+6)(x+6)-th light, so the xx-th light is always equal to the (x+6)(x+6)-th light.
-    Actually, the first 3 lights uniquely determine the rest of the sequence, as shown by the table below for performing the operations a, b, c, d:
 
-Light 1 = 1 + a + c + d
-Light 2 = 1 + a + b
-Light 3 = 1 + a + c
-Light 4 = 1 + a + b + d
-Light 5 = 1 + a + c
-Light 6 = 1 + a + b
-So 1,2,3 decided the other
+The four buttons:
 
-Light 4 = (Light 1) + (Light 2) + (Light 3)
-Light 5 = Light 3
-Light 6 = Light 2
+Flip all the lights.
+Flip lights with even numbers.
+Flip lights with odd numbers.
+Flip lights with (3k + 1) numbers, k = 0, 1, 2, ...
+If we use button 1 and 2, it equals to use button 3.
+Similarly...
+
+1 + 2 --> 3, 1 + 3 --> 2, 2 + 3 --> 1
+So, there are only 8 cases.
+
+All_on, 1, 2, 3, 4, 1+4, 2+4, 3+4
+
+and first 3 lights determine the rest
 
      */
     public int flipLights(int n, int m) {
