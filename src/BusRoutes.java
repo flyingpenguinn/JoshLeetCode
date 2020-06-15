7040ede762d700876bdf6647ea1cf09d246d2159
flyingpenguinn <zjm84812@gmail.com>
2020-06-15 16:03:24 -0400

    333

--- src/BusRoutes.java
+++ src/BusRoutes.java
@@ -7,102 +7,100 @@ import java.io.IOException;
 import java.util.*;
 
 public class BusRoutes {
-
-    class Node {
-        int value;
-        int ith;
-        int dist;
-
-        public Node(int value, int ith, int dist) {
-            this.value = value;
-            this.ith = ith;
-            this.dist = dist;
-        }
-    }
-
-    // filter useless nodes that are already populated.
-    // in bfs we can skip checking some edges when they don't make difference. this will reduce the complexity from O(v+e) when e is huge
-    boolean[] ithset;
-    Set<Integer> stopset = new HashSet<>();
-
-    Map<Integer, Set<Integer>> con = new HashMap<>();
-
-    // m = rows, n = max row lenth
-    // there are at most m*n nodes in queue
-    // we at most check m*n nodes when we traverse the queue. so overall time is Omn
-    public int numBusesToDestination(int[][] routes, int s, int t) {
-        int n = routes.length;
-        ithset = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < routes[i].length; j++) {
-                Set<Integer> curcon = con.getOrDefault(routes[i][j], new HashSet<>());
-                curcon.add(i);
-                con.put(routes[i][j], curcon);
-            }
-        }
-        Set<Integer> starter = con.getOrDefault(s, new HashSet<>());
-        if (starter.isEmpty()) {
-            return -1;
-        }
-        Set<Integer> ender = con.getOrDefault(t, new HashSet<>());
-        if (ender.isEmpty()) {
-            return -1;
-        }
+    // the gist is to enumerate on bus not stop...same complexity as leetcode solution, but looks suspicious as the intersect part makes the solution n^2*len of bus route in theory
+    public int numBusesToDestination(int[][] a, int s, int t) {
         if (s == t) {
             return 0;
         }
-        int targetNode = t;
-        int startNode = s;
-        Deque<Node> q = new ArrayDeque<>();
-        for (int si : con.get(startNode)) {
-            Node node = new Node(startNode, si, 1);
-            q.offer(node);
-            stopset.add(startNode);
+        int n = a.length;
+
+        Map<Integer, Set<Integer>> bustostop = new HashMap<>();
+        Deque<int[]> q = new ArrayDeque<>();
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                int stop = a[i][j];
+                int bus = i;
+                bustostop.computeIfAbsent(bus, k -> new HashSet<>()).add(stop);
+                if (a[i][j] == s) {
+                    q.offerLast(new int[]{i, 1});
+                    seen.add(i);
+                }
+            }
+        }
+        Map<Integer, Set<Integer>> nextbus = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (intersect(bustostop.get(i), bustostop.get(j))) {
+                    nextbus.computeIfAbsent(i, k -> new HashSet<>()).add(j);
+                    nextbus.computeIfAbsent(j, k -> new HashSet<>()).add(i);
+                }
+            }
         }
 
         while (!q.isEmpty()) {
-            Node top = q.poll();
-            //  System.out.println(top.value);
-            int ith = top.ith;
-            int topdist = top.dist;
-            // zero first 1 next we don't need a priority queue
-            if (!ithset[ith]) {
-                ithset[ith] = true;
-                int[] group = routes[ith];
-                for (int val : group) {
-                    if (val == top.value) {
-                        continue;
-                    }
-                    Node groupNode = new Node(val, ith, topdist);
-                    if (groupNode.value == targetNode) {
-                        return topdist;
-                    }
-                    q.offer(groupNode);
-                }
+            int[] top = q.poll();
+            int curbus = top[0];
+            int dist = top[1];
+            if (bustostop.getOrDefault(curbus, new HashSet<>()).contains(t)) {
+                return dist;
             }
-            if (!stopset.contains(top.value)) {
-                stopset.add(top.value);
-                Set<Integer> others = con.getOrDefault(top.value, new HashSet<>());
-                for (int other : others) {
-                    if (other == ith) {
-                        continue;
-                    }
-                    Node othergroupNode = new Node(top.value, other, topdist + 1);
-                    if (othergroupNode.value == targetNode) {
-                        return othergroupNode.dist;
-                    }
-                    q.offer(othergroupNode);
+            for (Integer next : nextbus.getOrDefault(curbus, new HashSet<>())) {
+                if (seen.add(next)) {
+                    q.offerLast(new int[]{next, dist + 1});
                 }
             }
         }
         return -1;
     }
 
-    public static void main(String[] args) throws IOException {
-        String file = "E:\\dev\\project\\JoshLeet\\tests\\busroutes.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(new File(file)));
-        String s = reader.readLine();
-        int[][] routes = ArrayUtils.readUnEven(s);
-        System.out.println(new BusRoutes().numBusesToDestination(routes, 0, 90000));
+    private boolean intersect(Set<Integer> s1, Set<Integer> s2) {
+        for (int se : s1) {
+            if (s2.contains(se)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class BusRouteSimplerBfs {
+    // visit as a level order in a bfs way.
+    // note we dont expand a bus again if we have done so
+    public int numBusesToDestination(int[][] a, int s, int t) {
+        int n = a.length;
+        Map<Integer, Set<Integer>> stoptobus = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                int stop = a[i][j];
+                int bus = i;
+                stoptobus.computeIfAbsent(stop, k -> new HashSet<>()).add(bus);
+            }
+        }
+        // stop and buses taken at this point
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{s, 0});
+        Set<Integer> seenstop = new HashSet<>();
+        seenstop.add(s);
+        Set<Integer> seenbus = new HashSet<>();
+        while (!q.isEmpty()) {
+            int[] top = q.poll();
+            int stop = top[0];
+            int dist = top[1];
+            if (stop == t) {
+                return dist;
+            }
+            Set<Integer> buses = stoptobus.getOrDefault(stop, new HashSet<>());
+            for (int bus : buses) {
+                if (seenbus.add(bus)) {// important: if we expanded this bus already, dont do it again
+                    for (int nextstop : a[bus]) {
+                        if (seenstop.add(nextstop)) {
+                            q.offer(new int[]{nextstop, top[1] + 1});
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
