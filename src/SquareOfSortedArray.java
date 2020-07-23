cb45bf66af7adef6322f4f5e9dcf097e95d92864
flyingpenguinn <zjm84812@gmail.com>
2020-07-22 21:05:54 -0400

    ttt

--- src/SquareOfSortedArray.java
+++ src/SquareOfSortedArray.java
@@ -2,61 +2,23 @@ import java.util.Arrays;
 
 public class SquareOfSortedArray {
     public int[] sortedSquares(int[] a) {
-        // we can also do it reversed so we dont need to binary search 0 first
-        // r[size--] = xxx, one pointer from 0, the other from end of the array
-        int biggestNegative = binarySearchFirstSmaller(a, 0);
-        if (biggestNegative == -1) {
-            for (int i = 0; i < a.length; i++) {
-                a[i] *= a[i];
-            }
-            return a;
-        } else {
-
-            int[] r = new int[a.length];
-            int np = biggestNegative;
-            int pp = biggestNegative + 1;
-            int rp = 0;
-            while (np >= 0 && pp < a.length) {
-                int nps = a[np] * a[np];
-                int pps = a[pp] * a[pp];
-                if (pps < nps) {
-                    r[rp++] = pps;
-                    pp++;
-                } else {
-                    r[rp++] = nps;
-                    np--;
-                }
-            }
-            while (np >= 0) {
-                int nps = a[np] * a[np];
-                r[rp++] = nps;
-                np--;
-            }
-            while (pp < a.length) {
-                int pps = a[pp] * a[pp];
-                r[rp++] = pps;
-                pp++;
-            }
-            return r;
+        if (a == null || a.length == 0) {
+            return new int[0];
         }
-
-    }
-
-    private int binarySearchFirstSmaller(int[] a, int target) {
-        int l = 0;
-        int u = a.length - 1;
-        while (l <= u) {
-            int mid = l + (u - l) / 2;
-            if (a[mid] >= target) {
-                u = mid - 1;
+        int n = a.length;
+        int[] r = new int[n];
+        int ri = n - 1;
+        int i = 0;
+        int j = n - 1;
+        while (ri >= 0) {
+            if (Math.abs(a[i]) > Math.abs(a[j])) {
+                r[ri--] = a[i] * a[i];
+                i++;
             } else {
-                l = mid + 1;
+                r[ri--] = a[j] * a[j];
+                j--;
             }
         }
-        return u;
-    }
-
-    public static void main(String[] args) {
-
+        return r;
     }
 }
