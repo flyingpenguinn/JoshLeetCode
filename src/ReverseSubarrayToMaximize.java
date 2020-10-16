be26e8dba122b935761321abca362fba911a7a36
flyingpenguinn <zjm84812@gmail.com>
2020-10-16 11:33:13 -0400

    Update ReverseSubarrayToMaximize.java

--- src/ReverseSubarrayToMaximize.java
+++ src/ReverseSubarrayToMaximize.java
@@ -37,63 +37,56 @@ public class ReverseSubarrayToMaximize {
 
 https://leetcode.com/problems/reverse-subarray-to-maximize-array-value/discuss/489968/O(n)-Java-with-Mathematical-Proof
 
+sum = |a[i-1]-a[j]| -|a[i-1]-a[i]| +|a[i]-a[j+1]| -|a[j]-a[j+1]|
+
+given i, 2nd is decided
+
+we then take out the 4 possibilities out of
+|a[i-1]-a[j]|+|a[i]-a[j+1]|
+these are v1 to v4, we need max or min of them, + the 4th item above which is also decided by j
+
  */
-    int Min = -1000000000;
-
     public int maxValueAfterReverse(int[] a) {
-        int base = 0;
+
         int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            base += abs(a[i] - a[i + 1]);
-        }
-        int best = 0;
-        int[][] dp = new int[n][4];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Min);
-        }
+        long sum = 0;
         for (int i = 0; i + 1 < n; i++) {
+            sum += abs(a[i] - a[i + 1]);
+        }
+        // all -abs(a[j]-a[j+1])
+        long v1 = Integer.MAX_VALUE;
+        // min a[j]+a[j+1]
+        long v2 = Integer.MAX_VALUE;
+        // min a[j]-a[j+1]
+        long v3 = Integer.MIN_VALUE;
+        // max a[j]-a[j+1]
+        long v4 = Integer.MIN_VALUE;
+        // max a[j]+a[j+1]
 
-            int aad = abs(a[i] - a[i + 1]);
-            // |a-d |
-            if (i - 2 >= 0) {
-                // c<a, b<=a-2
-                int v1 = a[i] - a[i + 1] - aad;
-                // (a-d)- |a-d|
-                int c1 = dp[i - 2][0];
-                int v2 = -(a[i] + a[i + 1]) - aad;
-                // -(a+d)-|a-d|
-                int c2 = dp[i - 2][1];
-                int v3 = a[i] + a[i + 1] - aad;
-                // (a+d)- |a-d |
-                int c3 = dp[i - 2][2];
-                int v4 = -(a[i] - a[i + 1]) - aad;
-                // -(a-d) - |a-d
-                int c4 = dp[i - 2][3];
-                int m1 = v1 + c1;
-                int m2 = v2 + c2;
-                int m3 = v3 + c3;
-                int m4 = v4 + c4;
-                int cmax = max(m1, max(m2, max(m3, m4)));
-                best = max(best, cmax);
+        // calc special case i=0. note we dont need to switch from 0...n-1 that's the same...so from n-2
+        long max = 0;
+        for (int i = n - 2; i >= 1; i--) {
+            long cur = abs(a[0] - a[i + 1]) - abs(a[i] - a[i + 1]);
+            max = max(cur, max);
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            long cur = abs(a[i - 1] - a[n - 1]) - abs(a[i - 1] - a[i]); // j=n-1 has no a[j+1], it is special process it first
+            long minus = -abs(a[i - 1] - a[i]);
+            long c1 = a[i - 1] + a[i] - v1 + minus;
+            long c2 = a[i - 1] - a[i] - v2 + minus;
+            long c3 = a[i] - a[i - 1] + v3 + minus;
+            long c4 = -a[i] - a[i - 1] + v4 + minus;
+            cur = max(cur, max(c1, max(c2, max(c3, c4))));
+            max = max(max, cur);
+            if (i + 1 < n) {
+                v1 = min(v1, a[i] + a[i + 1] + abs(a[i] - a[i + 1]));
+                v2 = min(v2, a[i] - a[i + 1] + abs(a[i] - a[i + 1]));
+                v3 = max(v3, a[i] - a[i + 1] - abs(a[i] - a[i + 1]));
+                v4 = max(v4, a[i] + a[i + 1] - abs(a[i] - a[i + 1]));
             }
-            dp[i][0] = max((i == 0 ? Min : dp[i - 1][0]), -(a[i] - a[i + 1]) - aad);
-            // -(b-c)- |b-c|
-            dp[i][1] = max((i == 0 ? Min : dp[i - 1][1]), (a[i] + a[i + 1]) - aad);
-            // (b+c)- |b-c|
-            dp[i][2] = max((i == 0 ? Min : dp[i - 1][2]), -(a[i] + a[i + 1]) - aad);
-            // (-(b+c) - |b-c|
-            dp[i][3] = max((i == 0 ? Min : dp[i - 1][3]), (a[i] - a[i + 1] - aad));
-            // (b-c)- |b-c|
-
         }
-        // to cover reversing on the two ends
-        for (int i = 1; i + 1 < n; i++) {
-            int m1 = abs(a[0] - a[i + 1]) - abs(a[i] - a[i + 1]);
-            int m2 = abs(a[n - 1] - a[i - 1]) - abs(a[i] - a[i - 1]);
-            int cmax = max(m1, m2);
-            best = max(best, cmax);
-        }
-        return base + best;
+        long rt = sum + max;
+        return (int) rt;
     }
 
     public static void main(String[] args) {
