1b7a3ae7ab4bea1cb582cbd3f445f3ee53294855
flyingpenguinn <zjm84812@gmail.com>
2020-10-23 21:06:27 -0400

    Update DivideTwoIntegers.java

--- src/DivideTwoIntegers.java
+++ src/DivideTwoIntegers.java
@@ -26,45 +26,44 @@ public class DivideTwoIntegers {
     // somewhat similar to binary lifting concept
     public int divide(int a, int b) {
         // b != 0
-        if (a == 0) {
+        if(a==0){
             return 0;
         }
-        if (b == -1 && a == Integer.MIN_VALUE) {
+        if(b==-1 && a==Integer.MIN_VALUE){
             return Integer.MAX_VALUE; // only possible overflow
         }
-        if (a < 0 && b > 0) {
+        if(a<0 && b>0){
             return -div(a, -b)[0];
         }
-        if (a > 0 && b < 0) {
+        if(a>0 && b<0){
             return -div(-a, b)[0];
         }
-        if (a > 0 && b > 0) {
+        if(a>0 && b>0){
             return div(-a, -b)[0];
         }
-        return div(a, b)[0];
+        return div(a,b)[0];
     }
 
-    private int[] div(int a, int b) {
+    private int[] div(int a, int b){
         // a and b both <0 to avoid overflow
-        if (a > b) {
+        if(a>b){
             // -2 vs -3, return 0, -2
             return new int[]{0, a};
         }
-        if (a % b == 0) {
-            return new int[]{a / b, 0};
+        if(a==b){
+            return new int[]{1,0};
         }
-        if (b < Integer.MIN_VALUE - b) {
+        if(b<Integer.MIN_VALUE-b){
             // -1028 vs -1000 and -1024 is the limit, return 1, -28
-            // note this can't be b+b<integer.min, because it will still overflow
-            return new int[]{1, a - b};
+            return new int[]{1, a-b};
         }
-        int[] later = div(a, b + b);
+        int[] later = div(a, b+b);
         int mod = later[1];
         int quot = later[0];
-        if (mod < b) { // won't == b
-            return new int[]{quot + quot + 1, mod - b};
-        } else {
-            return new int[]{quot + quot, mod};
+        if(mod <=b){ 
+            return new int[]{quot+quot+1, mod-b};
+        }else{
+            return new int[]{quot+quot, mod};
         }
     }
 
