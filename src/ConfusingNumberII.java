deda178e60428db5f6f04bd7baa3d1218f128862
flyingpenguinn <zjm84812@gmail.com>
2020-11-11 12:51:08 -0500

    tt

--- src/ConfusingNumberII.java
+++ src/ConfusingNumberII.java
@@ -35,58 +35,105 @@ Note:
 1 <= N <= 10^9
  */
 public class ConfusingNumberII {
-    int curlen = 0;
+    // all valid - strobo = confusing numbers
+    // all valid ~ 2^9*5, strobo is 5^(9/2)*5 because it only calculates half of the length
+    private int[] m = {0, 1, -1, -1, -1, -1, 9, -1, 8, 6};
 
-    public int confusingNumberII(int n) {
-        int r = 0;
-        String sn = String.valueOf(n);
-        int len = sn.length();
-        for (int i = 1; i <= len; i++) {
-            curlen = 0;
-            doc(i, n, 0, sn.charAt(0) - '0', true, i == len);
-            r += curlen;
+    // the strobo part is the same as strobo number III
+    public int strobos(long n) {
+        int len = String.valueOf(n).length();
+        long res = 0;
+        for (int l = 1; l <= len; l++) {
+            res += dfs(0, l, n, "", "");
         }
-        return r;
+        return (int) res;
     }
 
-    int[] cand = {0, 1, 6, 8, 9};
-    int[] map = {0, 1, -1, -1, -1, -1, 9, 7, 8, 6};
+    private long dfs(int i, int len, long n, String p1, String p2) {
+        int start = i > 0 || len == 1 ? 0 : 1;
+        long res = 0;
+        if (i == len / 2) {
+            if (len % 2 == 0) {
+                String str = p1 + p2;
+                if (Long.valueOf(str) <= n) {
+                    //     System.out.println(str);
+                    res++;
+                }
+            } else {
 
-    // i == remaining length
-    private void doc(int i, int limit, long cur, int firstnum, boolean first, boolean dofilter) {
-        if (i == 0) {
-            if (isconf(cur, limit)) {
-                curlen++;
+                for (int j = start; j < 10; j++) {
+                    if (m[j] == j) {
+                        String str = p1 + j + p2;
+                        if (Long.valueOf(str) <= n) {
+                            //     System.out.println(str);
+                            res++;
+                        }
+                    }
+                }
             }
-            return;
+            return res;
         }
-        for (int j = 0; j < cand.length; j++) {
-            if (first && cand[j] == 0) {
+        for (int j = start; j < 10; j++) {
+            if (m[j] != -1) {
+                res += dfs(i + 1, len, n, p1 + j, m[j] + p2);
+            }
+        }
+        return res;
+    }
+
+    // calculate all valid numbers
+    private int valids(int n) {
+        int len = String.valueOf(n).length();
+        long res = 0;
+        for (int l = 1; l < len; l++) {
+            int cur = quickvalid(l, l == 1);
+            //   System.out.println(cur);
+            res += cur;
+        }
+        res += findvalid(0, len, false, n);
+        return (int) res;
+    }
+
+    private int quickvalid(int l, boolean single) {
+        if (l == 1) {
+            return single ? 5 : 4;
+        }
+        return 5 * quickvalid(l - 1, false);
+    }
+
+    // can potentially dp to make it even faster
+    private int findvalid(int i, int l, boolean sm, int n) {
+        if (i == l) {
+            return 1;
+        }
+        if (sm) {
+            return 5 * findvalid(i + 1, l, true, n);
+        }
+        int start = i > 0 || l == 1 ? 0 : 1;
+        int nd = String.valueOf(n).charAt(i) - '0';
+        int res = 0;
+        int small = 0;
+        int eq = 0;
+        for (int j = start; j <= nd; j++) {
+            if (m[j] == -1) {
                 continue;
             }
-            if (first && cand[j] > firstnum && dofilter) {
-                break;
+            if (j < nd) {
+                small++;
+            } else {
+                eq++;
             }
-            doc(i - 1, limit, cur * 10 + cand[j], firstnum, false, dofilter);
         }
+
+        int cursmall = small * findvalid(i + 1, l, true, n);
+        //   System.out.println("cursmall = "+cursmall);
+        res += cursmall;
+        res += eq == 0 ? 0 : findvalid(i + 1, l, false, n);
+        return res;
     }
 
-    private boolean isconf(long cur, int n) {
-        if (cur > n) {
-            return false;
-        }
-        String scur = String.valueOf(cur);
-        int i = 0;
-        int j = scur.length() - 1;
-        while (i <= j) {
-            if (scur.charAt(j) - '0' == map[scur.charAt(i) - '0']) {
-                i++;
-                j--;
-            } else {
-                return true;
-            }
-        }
-        return false;
+    public int confusingNumberII(int n) {
+        return valids(n) - strobos(n);
     }
 
 
@@ -97,142 +144,53 @@ public class ConfusingNumberII {
         System.out.println(new ConfusingNumberII().confusingNumberII(1000000000));
 
     }
-
 }
 
-class ConfusingNumberDigitalQuicker {
-    // longer, vut takes only single digit ms
-    public int confusingNumberII(int num) {
-        char[] sn = String.valueOf(num).toCharArray();
-        int n = sn.length;
-        return conf(n, sn);
+class ConfusingNumberIIDirectDfs {
+    // O(5^9), note we should only use int to avoid expensive string operations
+    public int confusingNumberII(int n) {
+        map.put(0, 0);
+        map.put(1, 1);
+        map.put(6, 9);
+        map.put(8, 8);
+        map.put(9, 6);
+        dfs(0, String.valueOf(n).length(), 0, n);
+        return res;
     }
 
-    int allvalid(int i, int n) {
-        if (i == n - 1) {
-            return i != 0 ? 5 : 4;
+
+    private Map<Integer, Integer> map = new HashMap<>();
+    private int res = 0;
+
+
+    private void dfs(int i, int len, long cur, long n) {
+        if (cur > n) {
+            return;
         }
-        int c = i != 0 ? 5 : 4;
-        return c * allvalid(i + 1, n);
-    }
-
-    int conf(int n, char[] sn) {
-        int r = 0;
-        for (int i = 1; i < n; i++) {
-            int av = allvalid(0, i);
-            int as = allstrobo(0, i);
-            //        System.out.println(av + "-" + as);
-
-            r += av - as;
+        if (!strobo(cur)) {
+            //System.out.println(cur);
+            res++;
         }
-        int vn = validn(0, sn, false);
-        int snr = strobon(0, sn, false, true);
-        //     System.out.println(vn + "-" + snr);
-
-        r += vn - snr;
-        return r;
-    }
-
-    int allstrobo(int i, int n) {
-        if (i == n / 2) {
-            if (n % 2 == 0) {
-                return 1;
-            } else {
-                return i != 0 ? 3 : 2; //018
-            }
+        if (i == len) {
+            return;
         }
-        int c = i != 0 ? 5 : 4;
-        return c * allstrobo(i + 1, n);
-    }
-
-    int[] m = {0, 1, -2, -3, -4, -5, 9, -7, 8, 6};
-
-    // n=sn.len digit strobo num <=sn
-    // whether we allow bigger and eq number
-
-    int validn(int i, char[] sn, boolean ba) {
-        int n = sn.length;
-        if (i == n) {
-            return 1;
-        }
-        int vi = sn[i] - '0';
-        int r = 0;
-        int st = i != 0 ? 0 : 1;
-        for (int j = st; j <= 9; j++) {
-            if (m[j] < 0) {
+        for (int k : map.keySet()) {
+            if (i == 0 && k == 0 && len > 1) {
                 continue;
             }
-            if (ba) {
-                r += validn(i + 1, sn, ba);
-            } else {
-                if (j > vi) {
-                    break;
-                } else if (j < vi) {
-                    r += validn(i + 1, sn, true);
-                } else {
-                    r += validn(i + 1, sn, false);
-                }
-            }
+            dfs(i + 1, len, cur * 10 + k, n);
         }
-        return r;
     }
 
-    int strobon(int i, char[] sn, boolean ba, boolean ea) {
-        int n = sn.length;
-        int vi = sn[i] - '0';
-        int r = 0;
-        int st = i != 0 ? 0 : 1;
-        if (i == n / 2) {
-            if (n % 2 == 0) {
-                return ea ? 1 : 0;
-            } else {
-                for (int j = st; j <= 9; j++) {
-                    if (m[j] < 0 || m[j] != j) {
-                        continue;
-                    }
-                    if (ba) {
-                        r++;
-                    } else {
-                        if (j > vi) {
-                            break;
-                        } else if (j < vi) {
-                            r++;
-                        } else {
-                            r += ea ? 1 : 0;
-                        }
-                    }
-                }
-                return r;
-            }
+    private boolean strobo(long n) {
+        long reverse = 0;
+        long oldn = n;
+        // 168=> 891
+        while (n > 0) {
+            int mod = (int) n % 10;
+            reverse = reverse * 10 + map.get(mod);
+            n /= 10;
         }
-        for (int j = st; j <= 9; j++) {
-            if (m[j] < 0) {
-                continue;
-            }
-            if (ba) {
-                r += strobon(i + 1, sn, ba, ea);
-            } else {
-                if (j > vi) {
-                    break;
-                } else if (j < vi) {
-                    r += strobon(i + 1, sn, true, true);
-                } else {
-                    // j==vi
-                    int vk = sn[n - 1 - i] - '0';
-                    if (m[j] < vk) {
-                        r += strobon(i + 1, sn, ba, true);
-                        // 9...6 vs 9...7.
-                        // note ea = true because at k... digits this number is smaller
-                    } else if (m[j] > vk) {
-                        r += strobon(i + 1, sn, ba, false);
-                        // 988..6 vs 988..5 must remember this 6>5 we must do a smaller number at some point
-                    } else {
-                        // otherwise keep looking at later ones we may have chance to plant a smaller number
-                        r += strobon(i + 1, sn, ba, ea);
-                    }
-                }
-            }
-        }
-        return r;
+        return reverse == oldn;
     }
 }
--- src/StrobogrammaticNumberIII.java
+++ src/StrobogrammaticNumberIII.java
@@ -18,183 +18,54 @@ Because the range might be a large number, the low and high numbers are represen
 public class StrobogrammaticNumberIII {
     // use bigger allowed and eq allowed to filter bad numbers
     // O(5^n/2) at most
-    int[] m = {0, 1, -2, -3, -4, -5, 9, -7, 8, 6};
-
-    public int strobogrammaticInRange(String low, String high) {
-        long ch = count(high);
-        long cl = count(low);
-        long rt = ch - cl;
-        if (isstrobo(low)) {
-            rt++;
+    private int[] m = {0, 1, -1, -1, -1, -1, 9, -1, 8, 6};
+    public int strobos(long n) {
+        int len = String.valueOf(n).length();
+        long res = 0;
+        for(int l = 1; l<=len; l++){
+            res += dfs(0, l,n, "", "");
         }
-        return rt < 0 ? 0 : (int) rt;
+        return (int)res;
     }
 
-    // strobo number from 0 to n inclusive
-    private long count(String n) {
-        int len = n.length();
-        long r = 0;
-        for (int i = 1; i < len; i++) {
-            r += allstrobo(i, false);
-        }
-        return r + nobigger(len, n, 0, false, true);
-    }
+    private long dfs(int i, int len, long n, String p1, String p2){
+        int start = i>0 || len==1? 0:1;
+        long res = 0;
+        if(i==len/2){
+            if(len % 2==0){
+                String str = p1+p2;
+                if(Long.valueOf(str)<=n){
+                    //     System.out.println(str);
+                    res++;
+                }
+            }else{
 
-    // strobo number <=s at range i...n-1-i
-    // biggercharallowed means whether we can have bigger chars than what s has
-    private long nobigger(int n, String s, int i, boolean biggercharallowed, boolean eqallowed) {
-        int r = 0;
-        int ni = s.charAt(i) - '0';
-        if (i == n / 2) {
-            if (n % 2 == 1) {
-                for (int j = 0; j <= 9; j++) {
-                    if (m[j] != j) {
-                        continue;
-                    }
-                    if (j < ni || (j > ni && biggercharallowed) || (j == ni && eqallowed)) {
-                        r++;
+                for(int j=start; j<10; j++){
+                    if(m[j]==j){
+                        String str = p1+j+p2;
+                        if(Long.valueOf(str)<=n){
+                            //     System.out.println(str);
+                            res++;
+                        }
                     }
                 }
-                return r;
-            } else {
-                return eqallowed ? 1 : 0;
-                // key: if eq has been invalid and we are just looking for < digits then 0
-                // this is the only place to check eqallowed
+            }
+            return res;
+        }
+        for(int j=start; j<10;j++){
+            if(m[j]!= -1){
+                res += dfs(i+1, len, n, p1+j, m[j]+p2);
             }
         }
-        int start = i == 0 ? 1 : 0;
-        for (int j = start; j <= 9; j++) {
-            if (m[j] < 0) {
-                continue;
-            }
-            if (biggercharallowed) {
-                r += nobigger(n, s, i + 1, true, true);
-            } else if (j < ni) {
-                r += nobigger(n, s, i + 1, true, true);
-            } else if (j > ni) {
-                break;
-            } else {
-                int vk = s.charAt(n - 1 - i) - '0';
-                if (m[j] > vk) {
-                    // s==6...0 vs 6...9  we are not going to allow eq any more
-                    r += nobigger(n, s, i + 1, false, false);
-                } else if (m[j] < vk) {
-                    // 999959. we will get 9..6<9..9 and this will make eqallowed true
-                    r += nobigger(n, s, i + 1, false, true);
-                } else {
-                    // s==8...8 vs 8...8, we will keep allowing eq if it is allowed before
-                    r += nobigger(n, s, i + 1, false, eqallowed);
-                }
-            }
-        }
-        return r;
+        return res;
     }
-
-    // all strobo numbers of length i
-    private long allstrobo(int i, boolean allowzero) {
-        if (i == 0) {
-            return 1;
-        }
-        if (i == 1) {
-            return 3;// 0,1,8
-        }
-        if (allowzero) {
-            return 5 * allstrobo(i - 2, true);
-        } else {
-            return 4 * allstrobo(i - 2, true);
-        }
-    }
-
-    private boolean isstrobo(String low) {
-        int i = 0;
-        int j = low.length() - 1;
-        while (i <= j) {
-            if (m[low.charAt(i++) - '0'] != (low.charAt(j--) - '0')) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new StroboNumberIIIEasier().strobogrammaticInRange("9695", "9697"));
-        /*System.out.println(new StrobogrammaticNumberIII().strobogrammaticInRange("0", "20000")); //54
-
-        System.out.println(new StrobogrammaticNumberIII().strobogrammaticInRange("0", "10000"));//39
-        System.out.println(new StrobogrammaticNumberIII().strobogrammaticInRange("0", "1680"));//21
-        System.out.println(new StrobogrammaticNumberIII().strobogrammaticInRange("50", "100"));//3
-        System.out.println(new StrobogrammaticNumberIII().strobogrammaticInRange("69", "100"));//3
-*/
-    }
-}
-
-
-class StroboNumberIIIEasier {
-    int curlen = 0;
 
     public int strobogrammaticInRange(String low, String high) {
-
-        long lhigh = Long.valueOf(high).longValue();
-        long llow = Long.valueOf(low).longValue();
-        if (llow > lhigh) {
+        long hn = Long.valueOf(high);
+        long ln = Long.valueOf(low);
+        if(hn<ln){
             return 0;
         }
-        int nhigh = strobo(lhigh);
-        int nlow = strobo(llow - 1);
-        return nhigh - nlow;
+        return strobos(hn) - strobos(ln-1);
     }
-
-    int strobo(long n) {
-        if (n < 0) {
-            return 0;
-        }
-        int r = 0;
-        String sn = String.valueOf(n);
-        int len = sn.length();
-        for (int i = 1; i <= len; i++) {
-            curlen = 0;
-            doc(i, i, n, "", "", sn.charAt(0) - '0', true, i == len);
-            r += curlen;
-        }
-        return r;
-    }
-
-    int[] m1 = {0, 1, 6, 8, 9};
-    int[] m2 = {0, 1, 9, 8, 6};
-
-    // i == remaining length
-    private void doc(int i, int len, long limit, String cur, String rcur, int firstnum, boolean first, boolean dofilter) {
-        if (i == 0) {
-            if (isgood(cur, rcur, limit)) {
-                curlen++;
-            }
-            return;
-        }
-        if (i == 1) {
-            for (int j = 0; j < m1.length; j++) {
-                if (m1[j] == m2[j]) {
-                    doc(i - 1, len, limit, cur + m1[j], cur, firstnum, false, dofilter);
-                }
-            }
-        } else {
-            for (int j = 0; j < m1.length; j++) {
-                if (first && m1[j] == 0 && len > 1) {
-                    continue;
-                }
-                if (first && m1[j] > firstnum && dofilter) {
-                    break;
-                }
-                doc(i - 2, len, limit, cur + m1[j], m2[j] + rcur, firstnum, false, dofilter);
-            }
-        }
-    }
-
-    private boolean isgood(String cur, String rcur, long limit) {
-        String rn = cur + rcur;
-        if (Long.valueOf(rn).longValue() <= limit) {
-            return true;
-        }
-        return false;
-    }
-
 }
