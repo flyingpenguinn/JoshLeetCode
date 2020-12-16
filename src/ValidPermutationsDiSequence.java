b9af3ad13a99963f756d976cc6963f73d247af92
flyingpenguinn <zjm84812@gmail.com>
2020-12-16 09:51:43 -0500

    Update ValidPermutationsDiSequence.java

--- src/ValidPermutationsDiSequence.java
+++ src/ValidPermutationsDiSequence.java
@@ -33,54 +33,45 @@ S consists only of characters from the set {'D', 'I'}.
 public class ValidPermutationsDiSequence {
     // O(n3). note to use j to indicate the ranking of the previous selected in the remaining numbers. note if its ranking was j, we can still pick
     // jth at this step when we are facing "I"
-    int MOD = 1000000007;
-    long[][] dp;
+    private int Mod = 1000000007;
+    private Long[][] dp;
 
     public int numPermsDISequence(String s) {
         int n = s.length();
-        dp = new long[n][n + 1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        dp = new Long[n][n + 1];
+        long res = 0;
+        for (int j = 0; j <= n; j++) {
+            res += don(0, j, s);
+            res %= Mod;
         }
-        // number is from 0...n
-        long r = 0;
-        for (int i = 0; i <= n; i++) {
-            // we iterate from the 2nd number
-            r += dom(0, i, s);
-            r %= MOD;
-        }
-        return (int) r;
+        return (int) res;
     }
 
-    // we are at position i, last number is the jth smallest in the remaining numbers
-    private long dom(int i, int j, String s) {
+    // at seq pos i, the last num picked is jth in the available numbers (the number j itself included)
+    private long don(int i, int j, String s) {
         int n = s.length();
         if (i == n) {
-            return 1;
+            return 1L;
         }
-        if (dp[i][j] != -1) {
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
-        int rt = 0;
-        if (s.charAt(i) == 'D') {
-            for (int k = 0; k < j; k++) {
-                rt += dom(i + 1, k, s);
-                rt %= MOD;
+        int rem = n - i - 1;// at ith seq pos we have n-i-1 numbers to deal with. note when we are at 0, we already took away 1 number
+        long res = 0;
+        if (s.charAt(i) == 'I') {
+            // gotcha is if it's I, we start from the jth from the remaining because we are taking this one out
+            for (int k = j; k <= rem; k++) {
+                res += don(i + 1, k, s);
+                res %= Mod;
             }
         } else {
-            // prev number would have ranked j. so new j here is bigger that that number
-            // when it's i-1 we only have 1 choice as 0...
-            for (int k = j; k < n - i; k++) {
-                rt += dom(i + 1, k, s);
-                rt %= MOD;
+            for (int k = 0; k < j; k++) {
+                res += don(i + 1, k, s);
+                res %= Mod;
             }
         }
-        dp[i][j] = rt;
-        return rt;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new ValidPermutationFroDiOptimized().numPermsDISequence("DID"));
+        dp[i][j] = res;
+        return res;
     }
 }
 
