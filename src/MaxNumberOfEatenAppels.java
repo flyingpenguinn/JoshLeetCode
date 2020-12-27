3a79ecff2c0c0bdb06baa1f883e562c1762a1716
flyingpenguinn <zjm84812@gmail.com>
2020-12-27 09:19:04 -0500

    Update MaxNumberOfEatenAppels.java

--- src/MaxNumberOfEatenAppels.java
+++ src/MaxNumberOfEatenAppels.java
@@ -16,7 +16,7 @@ public class MaxNumberOfEatenAppels {
         }
         int res = 0;
         for (int i = 0; i <= maxd; i++) {
-            // rotten appels...
+            // rotten apples...
             while (!pq.isEmpty()) {
                 if (pq.peek()[1] == i) {
                     pq.poll();
@@ -35,6 +35,36 @@ public class MaxNumberOfEatenAppels {
                 if (pq.peek()[0] == 0) {
                     pq.poll();
                 }
+            }
+        }
+        return res;
+    }
+}
+
+class MaxNumberOfEatenApplesIntervalMergeWrong {
+    // submitted and ACed in contest, but actually wrong. can't pass [1,1]  [9,2]. accu for 0 is intertwined with that of 1
+    // but is there a merge interval solution at all?
+    public int eatenApples(int[] a, int[] d) {
+
+        int[] apps = new int[200001];
+        for (int i = 0; i < a.length; i++) {
+            apps[i] += a[i];
+            apps[i + d[i]] -= a[i];
+        }
+        int sum = 0;
+        int res = 0;
+        int accu = 0;// in this sprint how many did we eat- we will need to discount these when apps[i] is <0
+        for (int i = 0; i < apps.length; i++) {
+            if (apps[i] < 0) {
+                sum += apps[i] + accu;
+                accu = 0;
+            } else {
+                sum += apps[i];
+            }
+            if (sum - res > 0) {
+                res++;
+                // note that we ate apples so that when we negate we pay these back
+                accu++;
             }
         }
         return res;
