7d8b701277ab2158c3ad5bac8c86a937c3676cd3
flyingpenguinn <zjm84812@gmail.com>
2021-01-08 10:26:19 -0500

    Update CheckIfTwoStringArraysAreEqual.java

--- src/CheckIfTwoStringArraysAreEqual.java
+++ src/CheckIfTwoStringArraysAreEqual.java
@@ -4,22 +4,21 @@ public class CheckIfTwoStringArraysAreEqual {
         int i2 = 0;
         int j1 = 0;
         int j2 = 0;
-        while (i1 < w1.length && j1 < w2.length) {
-            if (w1[i1].charAt(i2) == w2[j1].charAt(j2)) {
-                i2++;
-                j2++;
-            } else {
+        while(i1<w1.length && j1<w2.length){
+            if(w1[i1].charAt(i2) != w2[j1].charAt(j2)){
                 return false;
             }
-            if (i2 == w1[i1].length()) {
+            i2++;
+            j2++;
+            while(i1<w1.length && i2==w1[i1].length()){
                 i1++;
-                i2 = 0;
+                i2=0;
             }
-            if (j2 == w2[j1].length()) {
+            while(j1<w2.length && j2==w2[j1].length()){
                 j1++;
                 j2 = 0;
             }
         }
-        return true;
+        return i1==w1.length && j1 == w2.length;
     }
 }
