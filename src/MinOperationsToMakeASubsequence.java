5ba1b91c7fa0915d6aa7677870d35d8c261d36c2
flyingpenguinn <zjm84812@gmail.com>
2021-02-12 00:23:16 -0500

    Update MinOperationsToMakeASubsequence.java

--- src/MinOperationsToMakeASubsequence.java
+++ src/MinOperationsToMakeASubsequence.java
@@ -3,32 +3,39 @@ import java.util.*;
 public class MinOperationsToMakeASubsequence {
     // actually finding LCS. but it's n^2
     // LCS can be converted to LIS and solve in nlogn if one of the string is of unique values!  we convert the other array to indxes of the first
-    public int minOperations(int[] a, int[] b) {
+    public int minOperations(int[] t, int[] a) {
         Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < a.length; i++) {
-            m.put(a[i], i);
+        for (int i = 0; i < t.length; i++) {
+            m.put(t[i], i);
         }
-        int[] b2 = new int[b.length];
-        for (int i = 0; i < b.length; i++) {
-            b2[i] = m.getOrDefault(b[i], -1);
-        }
-        // lis on b2
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < b2.length; i++) {
-            if (b2[i] != -1) {
-                int pos = Collections.binarySearch(list, b2[i]);
-                if (pos < 0) {
-                    pos = -(pos + 1);
-                }
-                //   System.out.println(list);
-                //  System.out.println(b2[i]+" "+pos);
+        int max = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (m.containsKey(a[i])) {
+                int index = m.get(a[i]);
+                int pos = binary(list, index);
                 if (pos == list.size()) {
-                    list.add(b2[i]);
+                    list.add(index);
                 } else {
-                    list.set(pos, b2[i]);
+                    list.set(pos, index);
                 }
+                max = Math.max(max, list.size());
             }
         }
-        return a.length - list.size();
+        return t.length - max;
+    }
+
+    private int binary(List<Integer> list, int index) {
+        int l = 0;
+        int u = list.size() - 1;
+        while (l <= u) {
+            int mid = l + (u - l) / 2;
+            if (list.get(mid) >= index) {
+                u = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 }
