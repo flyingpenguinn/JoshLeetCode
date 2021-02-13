cf58cc6ac2461dc0d1866186deebd443c2e9618f
flyingpenguinn <zjm84812@gmail.com>
2021-02-13 12:58:23 -0500

    ttt

--- src/MinAdjSwapsForKConsecutives.java
+++ src/MinAdjSwapsForKConsecutives.java
@@ -3,25 +3,56 @@ import base.ArrayUtils;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 public class MinAdjSwapsForKConsecutives {
-    // TODO: do it myself
-    public int minMoves(int[] nums, int k) {
-        ArrayList<Long> A = new ArrayList<Long>(), B = new ArrayList<Long>();
-        for (int i = 0; i < nums.length; ++i)
-            if (nums[i] == 1)
-                A.add((long) i);
-        long res = Long.MAX_VALUE;
-        B.add(0L);
-        for (int i = 0; i < A.size(); ++i)
-            B.add(B.get(i) + A.get(i));
-        for (int i = 0; i < A.size() - k + 1; ++i)
-            res = Math.min(res, B.get(i + k) - B.get(k / 2 + i) - B.get((k + 1) / 2 + i) + B.get(i));
-        res -= (k / 2) * ((k + 1) / 2);
-        return (int) res;
+    // "move to consecutive" can usually be converted to
+    // move to center
+    // then compensate for the extra moves
+    public int minMoves(int[] a, int k) {
+        if (k == 1) {
+            return 0;
+        }
+        int n = a.length;
+        List<Integer> ones = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                ones.add(i);
+            }
+        }
+        int[] sum = new int[ones.size()];
+        for (int i = 0; i < ones.size(); i++) {
+            sum[i] = (i == 0 ? 0 : sum[i - 1]) + ones.get(i);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = k - 1; i < ones.size(); i++) {
+            int head = i - k + 1;
+            if (k % 2 == 1) {
+                int mid = (head + i) / 2;
+                int left = getsum(sum, head, mid - 1);
+                int right = getsum(sum, mid + 1, i);
+                int r1 = right - left;
+                int r = (k - 1) / 2;
+                int r2 = r * (r + 1);  // 1,2,3...r on two sides
+                int cur = r1 - r2;
+                min = Math.min(min, cur);
+            } else {
+                int mid = (head + i) / 2;
+                int left = getsum(sum, head, mid - 1);
+                int right = getsum(sum, mid + 1, i);
+                int r1 = right - left - ones.get(mid); // extra -ones.get(mid) at position i
+                int r = (k - 2) / 2;
+                int r21 = r * (r + 1);
+                int r22 = r + 1;
+                int cur = r1 - r21 - r22;  // 1,2,3...r on two sides, plus an extra r+1 on the right
+                min = Math.min(min, cur);
+            }
+        }
+        return min;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new MinAdjSwapsForKConsecutives().minMoves(ArrayUtils.read1d("1,0,0,0,0,0,1,1"), 3));
+    // sum from i... j
+    private int getsum(int[] sum, int i, int j) {
+        return i > j ? 0 : sum[j] - ((i == 0) ? 0 : sum[i - 1]);
     }
 }
--- src/ShortestPathInBinaryMatrix.java
+++ src/ShortestPathInBinaryMatrix.java
@@ -4,55 +4,37 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class ShortestPathInBinaryMatrix {
-    class Pos {
-        int i;
-        int j;
-        int dist;
+    // bfs. can be dijkastra, but can't dp
+    private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, 1}, {1, -1}, {1, 1}, {-1, -1}};
 
-        public Pos(int i, int j, int dist) {
-            this.i = i;
-            this.j = j;
-            this.dist = dist;
-        }
-    }
-
-    // bfs, can't dp
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        int r = grid.length;
-        if (r == 0) {
-            return 0;
-        }
-
-        int c = grid[0].length;
-        if (grid[0][0] == 1) {
+    public int shortestPathBinaryMatrix(int[][] a) {
+        if (a[0][0] == 1) {
             return -1;
         }
-        Deque<Pos> q = new ArrayDeque<>();
-        q.offer(new Pos(0, 0, 1));
+        int m = a.length;
+        int n = a[0].length;
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 1});
+        a[0][0] = 2;
         while (!q.isEmpty()) {
-            Pos top = q.pop();
-            if (top.i == r - 1 && top.j == c - 1) {
-                return top.dist;
+            int[] top = q.poll();
+            int i = top[0];
+            int j = top[1];
+            int cd = top[2];
+            if (i == m - 1 && j == n - 1) {
+                return cd;
             }
             for (int[] d : dirs) {
-                int nr = top.i + d[0];
-                int nc = top.j + d[1];
-                if (inRange(nr, nc, grid) && grid[nr][nc] == 0) {
-                    grid[nr][nc] = 2;
-                    q.offer(new Pos(nr, nc, top.dist + 1));
+                int ni = i + d[0];
+                int nj = j + d[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && a[ni][nj] == 0) {
+                    a[ni][nj] = 2;
+                    int ndist = cd + 1;
+                    q.offer(new int[]{ni, nj, ndist});
+
                 }
             }
         }
         return -1;
-    }
-
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-
-    boolean inRange(int r, int c, int[][] m) {
-        return r >= 0 && r < m.length && c >= 0 && c < m[0].length;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new ShortestPathInBinaryMatrix().shortestPathBinaryMatrix(ArrayUtils.read("[[0,0,0],[1,1,0],[1,1,1]]")));
     }
 }
