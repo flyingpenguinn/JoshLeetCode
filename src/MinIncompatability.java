fc4a6fd61d52466440f872d136a718b396167108
flyingpenguinn <zjm84812@gmail.com>
2021-02-19 12:23:06 -0500

    Update MinIncompatability.java

--- src/MinIncompatability.java
+++ src/MinIncompatability.java
@@ -4,57 +4,68 @@ public class MinIncompatability {
     // dp[size] = dp[sth]+dp[size-sth]  when both size and sth can divide t
     // in the worst case we need to iterate through c16/8 = 12870 items and calc their subset. that's roughly 3^12 so we were able to AC
     // we could have used similar approach as "k subset sum" but here i fear status can't really represent what we have chosen in this cycle though it passes AC...
+    private Integer[] dp;
     private int Max = 1000000;
-
     public int minimumIncompatibility(int[] a, int k) {
         int n = a.length;
-        if (n == k) {
+        if(k==n){
             return 0;
         }
-        int t = n / k;
-        int[] dp = new int[1 << n];
-        Arrays.fill(dp, Max);
-        for (int i = ((1 << t) - 1); i < (1 << n); i++) {
-            int bits = Integer.bitCount(i);
-            if (bits == t) {
-                if (!valid(i, a)) {
-                    continue;
-                }
-                int min = Max;
+
+        if(n%k != 0){
+            return -1;
+        }
+        dp = new Integer[(1<<n)];
+        int size = n/k;
+        int[] diff = new int[(1<<n)];
+        Arrays.fill(diff, Max);
+        for(int i=1; i<(1<<n); i++ ){
+            int csize = Integer.bitCount(i);
+            if(csize % size == 0){
                 int max = 0;
-                for (int j = 0; j < n; j++) {
-                    if (((i >> j) & 1) == 1) {
-                        min = Math.min(min, a[j]);
+                int min = Max;
+                boolean[] seen = new boolean[17];
+                boolean bad = false;
+                for(int j=0; j<n; j++){
+                    if( ((i>>j)&1) == 1){
+                        if(seen[a[j]]){
+                            bad = true;
+                            break;
+                        }
+                        seen[a[j]] = true;
                         max = Math.max(max, a[j]);
+                        min = Math.min(min, a[j]);
                     }
                 }
-                dp[i] = max - min;
-            } else if (bits % t == 0) {
-                for (int j = i; j > 0; j = ((j - 1) & i)) {
-                    int bitsj = Integer.bitCount(j);
-                    if (bitsj % t == 0) {
-                        int p1 = dp[j];
-                        int p2 = dp[i - j];
-                        dp[i] = Math.min(dp[i], p1 + p2);
-                    }
+                if(!bad){
+                    diff[i] = max - min;
                 }
             }
         }
-        int rt = dp[(1 << n) - 1];
-        return rt >= Max ? -1 : rt;
+        int rt = dfs((1<<n)-1, size, diff);
+        return rt>=Max? -1: rt;
     }
 
-    private boolean valid(int st, int[] a) {
-        int n = a.length;
-        boolean[] seen = new boolean[17];
-        for (int i = 0; i < n; i++) {
-            if (((st >> i) & 1) == 1) {
-                if (seen[a[i]]) {
-                    return false;
+    private int dfs(int i, int size, int[] diff){
+        int isize = Integer.bitCount(i);
+        if(isize == size){
+            return diff[i];
+        }
+        if(dp[i]!= null){
+            return dp[i];
+        }
+        int min = Max;
+        for(int j = i-1; j>0; j= (j-1) & i){
+            int jsize = Integer.bitCount(j);
+            if(jsize % size == 0 && diff[j] < Max){
+                int later = dfs(i-j, size, diff);
+                if(later < Max){
+                    int cur = diff[j] + later;
+                    min = Math.min(min, cur);
                 }
-                seen[a[i]] = true;
             }
         }
-        return true;
+        dp[i] = min;
+        return min;
     }
 }
