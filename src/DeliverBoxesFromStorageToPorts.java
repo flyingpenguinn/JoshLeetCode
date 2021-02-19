da14bc2a80e0fd70371537fca983ef81fe1b4eb7
flyingpenguinn <zjm84812@gmail.com>
2021-02-19 00:12:41 -0500

    Update DeliverBoxesFromStorageToPorts.java

--- src/DeliverBoxesFromStorageToPorts.java
+++ src/DeliverBoxesFromStorageToPorts.java
@@ -1,40 +1,38 @@
 public class DeliverBoxesFromStorageToPorts {
 
-    // dp i is the cost from 0 to i-1  dp i = dp start+ diffs between them +2 (come and go), dp[0]=0
-    public int boxDelivering(int[][] a, int portsCount, int mb, int mw) {
+    public int boxDelivering(int[][] a, int pn, int maxBoxes, int maxWeight) {
         int n = a.length;
-        int[] dp = new int[n + 1];
-        int[] diff = new int[n];
-        for (int i = 0; i + 1 < n; i++) {
-            diff[i] = (a[i][0] != a[i + 1][0]) ? 1 : 0;
+        // dp[i] is the cost from 0...i
+        int[] dp = new int[n];
+        int j = 0;
+        int w = 0;
+        // extra ports to travel at each i. note for 0, no extra port to trave so =0
+        int[] stops = new int[n];
+        stops[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i][0] != a[i - 1][0]) {
+                stops[i] = stops[i - 1] + 1;
+            } else {
+                stops[i] = stops[i - 1];
+            }
         }
-        int start = 0;
-        int cw = 0;
-        int cdiff = 0;
-        for (int i = 1; i <= n; i++) {
-            // we are planning to do from start...i-1
-            if (i - start > mb) {
-                cw -= a[start][1];
-                // if start... i-1 is too many boxes, drop start
-                cdiff -= diff[start++];
+        for (int i = 0; i < n; i++) {
+            // j...i, so i-j+1 is the box count. note we should add w here first
+            w += a[i][1];
+            int boxes = i - j + 1;
+            if (boxes > maxBoxes) {
+                w -= a[j++][1];
             }
-            cw += a[i - 1][1];
-            if (i - 2 >= 0) {
-                // i-2 is the box diff that we newly introduce: if we introduce box 1 we introduce diff[0] but i will be 2
-                cdiff += diff[i - 2];
+            while (w > maxWeight) {
+                w -= a[j++][1];
             }
-            while (cw > mw) {
-                cw -= a[start][1];
-                cdiff -= diff[start++];
+            while (j < i && dp[j] == (j == 0 ? 0 : dp[j - 1])) {
+                w -= a[j++][1];
             }
-            // we are calcing dp[i]
-            while (start + 1 < i && dp[start] == dp[start + 1]) {
-                cw -= a[start][1];
-                cdiff -= diff[start++];
-            }
-            dp[i] = dp[start] + 2 + cdiff;
-            //  System.out.println("i= "+i+" start= "+start+" cdiff= "+cdiff+" dp[i]= "+dp[i]);
+            // j... i so j<=i. we are guaranteed to start a new trip at j comparing to j-1
+            int cstops = stops[i] - stops[j];
+            dp[i] = (j == 0 ? 0 : dp[j - 1]) + cstops + 2;
         }
-        return dp[n];
+        return dp[n - 1];
     }
 }
