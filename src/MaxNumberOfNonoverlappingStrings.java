19326f0326b4203312f921d372bcf501be10fded
flyingpenguinn <zjm84812@gmail.com>
2021-03-16 20:05:45 -0400

    Update MaxNumberOfNonoverlappingStrings.java

--- src/MaxNumberOfNonoverlappingStrings.java
+++ src/MaxNumberOfNonoverlappingStrings.java
@@ -1,63 +1,56 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MaxNumberOfNonoverlappingStrings {
-    /*
 
-    1. find left and right of each char
-    2. if i > pending end we know we found a good pending string- the one ending at the "pendingend"
-    3. each time if we find a good pending we update pending end. in this way we can SPLIT a longer pending string to smaller ones
-
-     */
+    // 1. find good substrings first, at most 26 of them starting from a left
+    // 2. use normal interval way to pick the most non conflicting intervals
     public List<String> maxNumOfSubstrings(String s) {
-        int[] left = new int[26];
-        int[] right = new int[26];
-        Arrays.fill(left, -1);
-        Arrays.fill(right, -1);
         int n = s.length();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            int cind = c - 'a';
-            right[cind] = i;
-            if (left[cind] == -1) {
-                left[cind] = i;
-            }
-        }
-        int pendingEnd = -1;
-        String pending = "";
-        List<String> res = new ArrayList<>();
+        int[] right = new int[26];
+        int[] left = new int[26];
         for (int i = 0; i < n; i++) {
-            if (i > pendingEnd && !pending.isEmpty()) {
-                res.add(pending);
-                pending = "";
-            }
-            char c = s.charAt(i);
-            int cind = c - 'a';
-            if (i == left[cind]) {
-                int curRight = extendRight(i, right[cind], left, right, s);
-                if (curRight != -1) {
-                    pendingEnd = curRight;
-                    pending = s.substring(i, curRight + 1);
+            right[s.charAt(i) - 'a'] = i;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            left[s.charAt(i) - 'a'] = i;
+        }
+        List<int[]> intervals = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int cind = s.charAt(i) - 'a';
+            if (left[cind] == i) {
+                // doing this at most 26 times. for each start we find the end and validate if this is a valid substring
+                int cend = extend(s, i, right[cind], left, right);
+                if (cend != -1) {
+                    intervals.add(new int[]{i, cend});
                 }
             }
         }
-        if (!pending.isEmpty()) {
-            res.add(pending);
-            pending = "";
+        List<String> res = new ArrayList<>();
+        Collections.sort(intervals, (x, y) -> Integer.compare(x[1], y[1]));
+        int end = -1;
+        for (int i = 0; i < intervals.size(); i++) {
+            int cs = intervals.get(i)[0];
+            int ce = intervals.get(i)[1];
+            if (cs > end) {
+                res.add(s.substring(cs, ce + 1));
+                end = ce;
+            }
         }
         return res;
     }
 
-    private int extendRight(int start, int end, int[] left, int[] right, String s) {
+    private int extend(String s, int i, int j, int[] left, int[] right) {
 
-        for (int i = start; i <= end; i++) {
-            char c = s.charAt(i);
-            int cind = c - 'a';
-            if (left[cind] < start) {
+        int end = j;
+        for (int k = i; k <= end; k++) {
+            int cind = s.charAt(k) - 'a';
+            if (left[cind] < i) {
                 return -1;
-            } else {
-                end = Math.max(end, right[cind]);
+            } else if (right[cind] > end) {
+                end = right[cind];
             }
         }
         return end;
