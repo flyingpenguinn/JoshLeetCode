897833afb04c2f03b24d6171a9a960be11e533ca
flyingpenguinn <zjm84812@gmail.com>
2021-06-10 11:41:16 -0400

    Update MyCalendarI.java

--- src/MyCalendarI.java
+++ src/MyCalendarI.java
@@ -4,28 +4,23 @@ import java.util.TreeSet;
 
 public class MyCalendarI {
 
-    public static void main(String[] args) {
-        MyCalendar mc = new MyCalendar();
-        mc.book(47, 50);
-        mc.book(33, 41);
-    }
-}
+    private class MyCalendar {
 
-class MyCalendar {
+        private TreeMap<Integer, Integer> calendar;
+        // non overlapping, can use this since each start will only have one end
 
-    private TreeMap<Integer, Integer> calendar;
-    // non overlapping, can use this since each start will only have one end
-
-    public MyCalendar() {
-        calendar = new TreeMap<>();
-    }
-
-    public boolean book(int start, int end) {
-        Integer lowerStart = calendar.lowerKey(end);
-        if (lowerStart == null || calendar.get(lowerStart) <= start) {
-            calendar.put(start, end);
-            return true;
+        public MyCalendar() {
+            calendar = new TreeMap<>();
         }
-        return false;
+
+        public boolean book(int start, int end) {
+            Integer lowerStart = calendar.lowerKey(end);
+            if (lowerStart == null || calendar.get(lowerStart) <= start) {
+                calendar.put(start, end);
+                return true;
+            }
+            return false;
+        }
     }
+
 }
