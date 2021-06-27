4d93b85968475e07005d4cb573579dfcd25d09c4
flyingpenguinn <zjm84812@gmail.com>
2021-06-27 10:05:11 -0400

    Update CountWaysToBuildRoomInAntColony.java

--- src/CountWaysToBuildRoomInAntColony.java
+++ src/CountWaysToBuildRoomInAntColony.java
@@ -1,75 +1,89 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.math.*;
 
 public class CountWaysToBuildRoomInAntColony {
-    //@TODO: write myself
-    int mod = 1000000007;
-    List<Integer> graph[];
-    int cnt[];
-    long fact[];
+    // we get results from subtrees... Rs1, Rs2...
+    // but they can interleave. among all n-1 subtree nodes, we have (n-1)! ways to permute them. but Ns1, Ns2... nodes' orders are fixed
+    // so we end up having Rcur = ((Ncur-1)!/Ns1*Ns2...) * Rs1*Rs2...
+    // then the trick is to use (b/a) % m  = b*a mod inverse m to get the mod result
 
-    public int waysToBuildRooms(int[] A) {
-        int n = A.length;
-        graph = new ArrayList[n];
-        cnt = new int[n];
-        Arrays.setAll(graph, e -> new ArrayList<>());
+    private final int Mod = 1000000007;
+    private int[] children;
 
-
-        fact = new long[A.length + 10];
-        fact[0] = fact[1] = 1;
-        for (int i = 2; i < fact.length; i++) {
-            fact[i] = fact[i - 1] * i;
-            fact[i] %= mod;
+    public int waysToBuildRooms(int[] prev) {
+        int n = prev.length;
+        children = new int[n];
+        factor = new long[n + 1];
+        initFact(n);
+        List<Integer>[] tree = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            tree[i] = new ArrayList<>();
         }
-
-
-        for (int i = 1; i < A.length; i++) {
-            int pre = A[i];
-            graph[pre].add(i);
+        for (int i = 1; i < n; i++) {
+            tree[prev[i]].add(i);
         }
-
-
-        long res[] = dfs(0);
-        return (int) (res[1] % mod);
+        long res = solve(0, tree, children);
+        return (int) res;
     }
 
-
-    public long[] dfs(int root) {
-        long res[] = new long[]{1, 0};
-        int cnt = 0;
-        List<long[]> list = new ArrayList<>();
-        for (int next : graph[root]) {
-            long v[] = dfs(next);
-            cnt += v[0];
-            list.add(v);
+    private long solve(int i, List<Integer>[] tree, int[] children) {
+        children[i] = 1;
+        long num = 1;
+        long denom = 1;
+        for (int ne : tree[i]) {
+            long cr = solve(ne, tree, children);
+            children[i] += children[ne];
+            num *= cr;
+            num %= Mod;
+            long fcc = factor[children[ne]];
+            denom *= fcc;
+            denom %= Mod;
         }
-
-        res[0] += cnt;
-        long com = 1;
-        for (long p[] : list) {
-            long choose = C(cnt, (int) (p[0]));
-            cnt -= p[0];
-            com = com * choose;
-            com %= mod;
-            com = com * p[1];
-            com %= mod;
-        }
-
-        res[1] = com;
-        return res;
+        num *= factor[children[i] - 1];
+        num %= Mod;
+        num *= modInverse(denom, Mod);
+        num %= Mod;
+        return num;
     }
 
+    private long[] factor;
 
-    public long C(int i, int j) { // C(20,3)=20!/(17!*3!)
-        long mod = 1000000007;
-        long a = fact[i];
-        long b = ((fact[i - j] % mod) * (fact[j] % mod)) % mod;
-        BigInteger B = BigInteger.valueOf(b);
-        long binverse = B.modInverse(BigInteger.valueOf(mod)).longValue();
-        return ((a) * (binverse % mod)) % mod;
+    private void initFact(int n) {
+        long last = 1L;
+        factor[0] = last;
+        for (int i = 1; i <= n; i++) {
+            long cur = last * i;
+            cur %= Mod;
+            factor[i] = cur;
+            last = cur;
+        }
+    }
 
+    // copied mod inverse code
+    private long modInverse(long a, long m) {
+        long m0 = m;
+        long y = 0, x = 1;
+        if (m == 1) {
+            return 0;
+        }
+        while (a > 1) {
+            // q is quotient
+            long q = a / m;
+            long t = m;
+            // m is remainder now, process
+            // same as Euclid's algo
+            m = a % m;
+            a = t;
+            t = y;
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+        // Make x positive
+        if (x < 0) {
+            x += m0;
+        }
+        return x;
     }
 }
