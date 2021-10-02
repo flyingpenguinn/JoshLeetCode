437e19681e67dd0443b6c801748b896831741106
flyingpenguinn <zjm84812@gmail.com>
2021-10-01 20:58:01 -0400

    Update NumberOfDigitOne.java

--- src/NumberOfDigitOne.java
+++ src/NumberOfDigitOne.java
@@ -9,28 +9,29 @@ Output: 6
 Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
  */
 public class NumberOfDigitOne {
+    // count 1s on 1, 10, 100th digit, etc
     public int countDigitOne(int n) {
-        long r = 0;
-        long base = 1;
-        long stub = 0;
-        while (n >= base) {
-            int dig = (int) (n / base) % 10;
-            stub = dig * base + stub;
-            long ones = 0;
-            if (dig != 1) {
-                ones = ((n - base) / (base * 10) + 1) * base;
-                // 232->( 232-10)/100+1 = 3 counts of ten's digit:10,110,210
-                // note we -base so that 10->0,12->1 at one's digit
-            } else {
-                ones = ((n - base) / (base * 10)) * base;
-                // no +1 ! 111-> (111-10)/100= 1 counts of ten's digit. as if we are treating 101 at ten's digit
-                ones += stub - base + 1;
-                // plus 11-10+1=2 counts at ten's digit
+        int t = 1; // can expand to any digit
+        long res = 0;
+        for (long base = 10; base / 10 <= n; base *= 10) {
+            long stub = base / 10;
+            long mybase = n / base * base + stub*t;
+            long curdigit = ((n / stub) % 10);
+            if (curdigit == t) {
+                res += n - mybase + 1;
+                // floor means we don't count this 1 but instead only count 1s below it. for example if n is 11, then we only count one 1 on tenth digit
+                res += Math.floor(n * 1.0 / base) * stub;
             }
-            r += ones;
-            base *= 10;
+            else if (curdigit <t ) {
+                // if it's a 0 same treatment on previous 1s
+                res += Math.floor(n * 1.0 / base) * stub;
+            }
+            else {
+                // if it's a 2 or above we know we can include corresponding 1s
+                res += Math.ceil(n * 1.0 / base) * stub;
+            }
         }
-        return (int) r;
+        return (int)res;
     }
 
     public static void main(String[] args) {
