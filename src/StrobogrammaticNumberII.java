6902ec63a380217c832e886fb8aa2fb2d5c2fca7
flyingpenguinn <zjm84812@gmail.com>
2021-11-23 23:12:51 -0500

    Update StrobogrammaticNumberII.java

--- src/StrobogrammaticNumberII.java
+++ src/StrobogrammaticNumberII.java
@@ -18,28 +18,38 @@ public class StrobogrammaticNumberII {
     private List<String> res = new ArrayList<>();
 
     public List<String> findStrobogrammatic(int n) {
-        dfs(0, n, "", "");
+        dfs(0, n, new char[n]);
         return res;
     }
 
-    private void dfs(int i, int n, String p1, String p2) {
-        int start = (i > 0 || n == 1) ? 0 : 1;
-        if (i == n / 2) {
-            if (n % 2 == 1) {
-                for (int j = start; j < 10; j++) {
-                    if (m[j] == j) { // excluding 6 and 9
-                        res.add(p1 + j + p2);
-                    }
-                }
-            } else {
-                res.add(p1 + p2);
-            }
+    private void dfs(int index, int n, char[] a) {
+        if (index >= (n + 1) / 2) {
+            res.add(new String(a));
             return;
         }
-        for (int j = start; j < 10; j++) {
-            if (m[j] != -1) { // can allow all 5
-                dfs(i + 1, n, p1 + j, m[j] + p2);
-            }
+        int start = 0;
+        if(n>1 && index==0){
+            start = 1;
         }
+        for (int i = start; i < m.length; ++i) {
+            if (m[i] == -1) {
+                continue;
+            }
+            if (index == n - 1 - index) {
+                if (m[i] == i) {
+                    a[index] = (char) (i + '0');
+                }else{
+                    continue;
+                }
+            } else {
+                a[index] = (char) (i + '0');
+                a[n - 1 - index] = (char) (m[i] + '0');
+            }
+            dfs(index + 1, n, a);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new StrobogrammaticNumberII().findStrobogrammatic(1));
     }
 }
