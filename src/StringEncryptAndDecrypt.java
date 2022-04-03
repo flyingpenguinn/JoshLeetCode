d88a8f7085fb7b079fae2dacc9bc1f6d3ad64b78
flyingpenguinn <zjm84812@gmail.com>
2022-04-03 14:27:00 -0400

    Update StringEncryptAndDecrypt.java

--- src/StringEncryptAndDecrypt.java
+++ src/StringEncryptAndDecrypt.java
@@ -4,6 +4,57 @@ import java.util.Map;
 import java.util.Set;
 
 public class StringEncryptAndDecrypt {
+    // dont really need trie...
+
+    class EncrypterQuicker {
+
+        private Map<Character, String> em = new HashMap<>();
+        private Map<String, Set<Character>> dm = new HashMap<>();
+        private Set<String> ds = new HashSet<>();
+
+        public EncrypterQuicker(char[] keys, String[] values, String[] dictionary) {
+            for (int i = 0; i < keys.length; ++i) {
+                em.put(keys[i], values[i]);
+                dm.computeIfAbsent(values[i], k -> new HashSet<>()).add(keys[i]);
+            }
+            for(String d: dictionary){
+                ds.add(d);
+            }
+        }
+
+        public String encrypt(String word1) {
+            StringBuilder res = new StringBuilder();
+            for(int i=0; i<word1.length(); ++i){
+                char c = word1.charAt(i);
+                res.append(em.get(c));
+            }
+            return res.toString();
+        }
+
+        public int decrypt(String word2) {
+            int res = 0;
+            for(String d: ds){
+                int j =0;
+                int i = 0;
+                boolean bad = false;
+                for(;i<word2.length() && j<d.length(); i+=2){
+                    String str = word2.substring(i, i+2);
+                    Set<Character> mapped = dm.getOrDefault(str, new HashSet<>());
+                    if(mapped.contains(d.charAt(j))){
+                        ++j;
+                    }else{
+                        bad = true;
+                        break;
+                    }
+                }
+                if(!bad && i==word2.length() && j==d.length()){
+                    ++res;
+                }
+            }
+            return res;
+        }
+    }
+
     class Encrypter {
 
         private class TN {
