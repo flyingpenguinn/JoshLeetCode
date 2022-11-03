323332aa64e33e0813e967bdaf654a68c8eba79f
flyingpenguinn <zjm84812@gmail.com>
2022-11-02 21:46:54 -0400

    Update LongestPalindromeByConcatTwoLetterWords.java

--- src/LongestPalindromeByConcatTwoLetterWords.java
+++ src/LongestPalindromeByConcatTwoLetterWords.java
@@ -2,43 +2,57 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestPalindromeByConcatTwoLetterWords {
+    private void update(Map<String, Integer> m, String k, int d) {
+        int nv = m.getOrDefault(k, 0) + d;
+        if (nv <= 0) {
+            m.remove(k);
+        } else {
+            m.put(k, nv);
+        }
+    }
+
     public int longestPalindrome(String[] words) {
         Map<String, Integer> m = new HashMap<>();
+        for (String w : words) {
+            update(m, w, 1);
+        }
         int res = 0;
-        int unpaired = 0;
-        for(String s: words){
-            String rev = new StringBuilder(s).reverse().toString();
-            if(s.equals(rev)){
-                if(m.containsKey(rev)){
-                    res += 4;
-                    update(m, rev, -1);
-                    --unpaired;
-                }else{
-                    update(m, rev, 1);
-                    ++unpaired;
+        int maxpalin = 0;
+        for (String s:m.keySet()){
+            int count = m.get(s);
+            if (count == 0) {
+                continue;
+            }
+            int scount = m.get(s);
+            String rs = new StringBuilder(s).reverse().toString();
+            if (s.equals(rs)) {
+                res += (scount / 2) * 2 * s.length();
+                if (scount % 2 == 1 && ispalin(s)) {
+                    maxpalin = Math.max(maxpalin, s.length());
                 }
-            }else{
-                if(m.containsKey(rev)){
-                    res +=4;
-                    update(m, rev, -1);
-                }else{
-                    update(m, s, 1);
+            } else if (m.getOrDefault(rs, 0)>0) {
+                int mincounts = Math.min(count, m.get(rs));
+                res += mincounts * 2 * s.length();
+                m.put(rs, 0);
+            } else {
+                if (ispalin(s)) {
+                    maxpalin = Math.max(maxpalin, s.length());
                 }
             }
-
+            m.put(s, 0);
         }
-        if(unpaired>0){
-            res +=2;
-        }
+        res += maxpalin;
         return res;
     }
 
-    private void update(Map<String,Integer> m, String k, int d){
-        int nv = m.getOrDefault(k, 0)+d;
-        if(nv<=0){
-            m.remove(k);
-        }else{
-            m.put(k, nv);
+    private boolean ispalin(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
         }
+        return true;
     }
 }
