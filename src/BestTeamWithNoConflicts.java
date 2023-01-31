a1a42aea3d659fa932f9cec57322aafd683e1fc9
flyingpenguinn <zjm84812@gmail.com>
2023-01-30 22:24:36 -0500

    Update BestTeamWithNoConflicts.java

--- src/BestTeamWithNoConflicts.java
+++ src/BestTeamWithNoConflicts.java
@@ -1,30 +1,26 @@
 import java.util.Arrays;
 
 public class BestTeamWithNoConflicts {
-    // not a heap problem... more like longest increasing subsequence!
+    // not a heap problem... not a binary search.. not a graph....once we sort by age, it is longest increasing subsequence!
     public int bestTeamScore(int[] s, int[] a) {
         int n = s.length;
         int[][] ps = new int[n][2];
         for (int i = 0; i < n; i++) {
-            ps[i][0] = s[i];
-            ps[i][1] = a[i];
+            ps[i][0] = a[i];
+            ps[i][1] = s[i];
         }
-        Arrays.sort(ps, (x, y) -> x[1] != y[1] ? Integer.compare(y[1], x[1]) : Integer.compare(y[0], x[0]));
-        // big age first. also big salary first so that we dont need to worry about same age
+        Arrays.sort(ps, (x, y) -> x[0] != y[0] ? Integer.compare(x[0], y[0]) : Integer.compare(x[1], y[1]));
+        // younger age first. for big age, we can pick any person score <= current person, that person is guaranteed to be younger
         int max = 0;
         int[] dp = new int[n];
-        // enumerate youngest
         for (int i = 0; i < n; i++) {
-            int score = ps[i][0];
-            int cmax = score;
-            int limit = score;
+            int score = ps[i][1];
+            dp[i] = score;
             for (int j = i - 1; j >= 0; j--) {
-                if (ps[j][0] >= limit) {
-                    int cur = score + dp[j];
-                    cmax = Math.max(cur, cmax);
+                if (ps[j][1] <= score) {
+                    dp[i] = Math.max(dp[i], score + dp[j]);
                 }
             }
-            dp[i] = cmax;
             max = Math.max(dp[i], max);
         }
         return max;
