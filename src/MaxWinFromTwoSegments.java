e13d5067d7e0947008bb0bfd386cc1f484e1af59
flyingpenguinn <zjm84812@gmail.com>
2023-02-06 21:59:42 -0500

    Update MaxWinFromTwoSegments.java

--- src/MaxWinFromTwoSegments.java
+++ src/MaxWinFromTwoSegments.java
@@ -5,50 +5,17 @@ import java.util.TreeMap;
 public class MaxWinFromTwoSegments {
     public int maximizeWin(int[] a, int k) {
         int n = a.length;
-        TreeMap<Integer, Integer> m = new TreeMap<>();
-        for (int ai : a) {
-            m.put(ai, m.getOrDefault(ai, 0) + 1);
-        }
-        //  System.out.println(m);
-        int accu = 0;
-        Map<Integer, Integer> dp = new HashMap<>();
-        Map<Integer, Integer> am = new HashMap<>();
-        int maxsingle = 0;
+        int[] dp = new int[n]; // max value for one segment
+        int j = 0;
         int res = 0;
-        for (int i : m.keySet()) {
-            accu += m.get(i);
-            am.put(i, accu);
-            Integer last = m.lowerKey(i - k);
-            int cur = 0;
-            if (last == null) {
-                cur = accu;
-            } else {
-                cur = accu - am.get(last);
-            }
-            maxsingle = Math.max(maxsingle, cur);
-            dp.put(i, maxsingle);
-            int before = last == null ? 0 : dp.get(last);
-            int cmax = cur + before;
-            //  System.out.println("i="+i+" cur="+cur+" cmax="+cmax+" before="+before);
-            res = Math.max(cmax, res);
-        }
-        return res;
-    }
-
-    /*
-    similar thinking process but we can simplify
-
-    public int maximizeWin(int[] A, int k) {
-        int res = 0, n = A.length, j = 0, dp[] = new int[n + 1];
-        for (int i = 0; i < n; ++i) {
-            while (A[j] < A[i] - k)
+        for(int i=0; i<n; ++i){
+            while(j<=i && a[i]-a[j] > k){
                 ++j;
-            dp[i + 1] = Math.max(dp[i], i - j + 1);
-            res = Math.max(res, i - j + 1 + dp[j]);
+            }
+            int cur = i-j+1;
+            res = Math.max(res, cur + (j==0?0:dp[j-1]));
+            dp[i] = Math.max( (i==0?0:dp[i-1]), cur);
         }
         return res;
     }
-
-
-     */
 }
