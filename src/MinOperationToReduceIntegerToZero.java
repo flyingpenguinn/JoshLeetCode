dd7a896f7be5cab1ce730875ecbe359a89106f57
flyingpenguinn <zjm84812@gmail.com>
2023-02-20 21:42:48 -0500

    Update MinOperationToReduceIntegerToZero.java

--- src/MinOperationToReduceIntegerToZero.java
+++ src/MinOperationToReduceIntegerToZero.java
@@ -1,16 +1,22 @@
 public class MinOperationToReduceIntegerToZero {
+    // go for the nearest pow of 2
     public int minOperations(int n) {
-        //    System.out.println(Integer.toBinaryString(n));
-        int way1 = Integer.bitCount(n);
-        if (way1 == 1) {
+        if (n == 0) {
+            return 0;
+        }
+        if ((n & (n - 1)) == 0) {
             return 1;
         }
-        for (int j = 0; j < 32; ++j) {
-            if (((n >> j) & 1) == 1) {
-                return Math.min(way1, Math.min(1 + minOperations(n + (1 << j)), 1 + minOperations(n - (1 << j))));
-            }
+        // System.out.println(n);
+        int p1 = (int) Math.ceil(Math.log(n) / Math.log(2));
+        int p2 = (int) Math.floor(Math.log(n) / Math.log(2));
+        //   System.out.println(p1+" "+p2);
+        int num1 = (int) Math.pow(2, p1);
+        int num2 = (int) Math.pow(2, p2);
+        if (num1 - n <= n - num2) {
+            return 1 + minOperations(num1 - n);
+        } else {
+            return 1 + minOperations(n - num2);
         }
-        return way1;
-
     }
 }
