7a7e3b0b8a7640917f376e0f6160ac3609a91891
flyingpenguinn <zjm84812@gmail.com>
2023-03-07 22:45:47 -0500

    Update ShortestWayFormString.java

--- src/ShortestWayFormString.java
+++ src/ShortestWayFormString.java
@@ -1,27 +1,30 @@
 public class ShortestWayFormString {
     public int shortestWay(String s, String t) {
-        int rt = 1 + dos(s, 0, t, 0);
-        return rt <= 0 ? -1 : rt;
-    }
-
-    int dos(String s, int si, String t, int ti) {
         int sn = s.length();
         int tn = t.length();
-        if (ti == tn) {
-            return 0;
-        }
-
-
-        for (int i = si; i < sn; i++) {
-            if (s.charAt(i) == t.charAt(ti)) {
-                return dos(s, i + 1, t, ti + 1);
+        int i = 0;
+        int j = 0;
+        int res= 0;
+        while(j<tn){
+            int k = i;
+            while(k<i+sn){
+                if(k % sn==0){
+                    ++res;
+                }
+                char sv = s.charAt(k%sn);
+                if(sv == t.charAt(j)){
+                    break;
+                }else{
+                    ++k;
+                }
+            }
+            if(k==i+sn){
+                return -1;
+            }else{
+                i = k+1;
+                ++j;
             }
         }
-        for (int i = 0; i < si; i++) {
-            if (s.charAt(i) == t.charAt(ti)) {
-                return 1 + dos(s, i + 1, t, ti + 1);
-            }
-        }
-        return Integer.MIN_VALUE;
+        return res;
     }
 }
