561d6cea836226c9004e21fd662415231dd13f24
flyingpenguinn <zjm84812@gmail.com>
2023-05-07 20:45:39 -0400

    Update MakeCostsOfPathEqualInBinaryTree.java

--- src/MakeCostsOfPathEqualInBinaryTree.java
+++ src/MakeCostsOfPathEqualInBinaryTree.java
@@ -1,4 +1,6 @@
 public class MakeCostsOfPathEqualInBinaryTree {
+    // compare with " Lowest Common Ancestor of Deepest Leaves"
+    // note we can also do top down - enlarge each node so that the max path sum it affects = the max path sum itself. then go on to enlarge later nodes
     private int res = 0;
 
     public int minIncrements(int n, int[] cost) {
