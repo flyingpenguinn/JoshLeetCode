cf9e8cee7110461d9dedead5027bfba2827fb444
flyingpenguinn <zjm84812@gmail.com>
2023-07-26 23:12:20 -0400

    Update MaxRunningTimeOfNComputers.java

--- src/MaxRunningTimeOfNComputers.java
+++ src/MaxRunningTimeOfNComputers.java
@@ -1,35 +1,18 @@
 import java.util.Arrays;
 
 public class MaxRunningTimeOfNComputers {
-    public long maxRunTime(int n, int[] b) {
+    // either use whole battery for piece the smaller ones together....
+    public long maxRunTime(long n, int[] b) {
         long l = 1;
-        long u = (long) 1e15;
+        long u = (long) 1e14;
         Arrays.sort(b);
         while (l <= u) {
             long mid = l + (u - l) / 2L;
-            int met = 0;
-            long rem = mid;
-            int i = 0;
-            int[] cb = Arrays.copyOf(b, b.length);
-            while (met < n && i < cb.length) {
-                int remb = cb.length - i;
-                if (n - met > remb) {
-                    // we are not going to make it
-                    break;
-                }
-                if (cb[i] >= rem) {
-                    cb[i] -= rem;
-                    rem = mid;
-                    ++met;
-                } else {
-                    if (n - met == remb) {
-                        break;// can't borrow this one any more
-                    }
-                    rem -= cb[i];
-                    ++i;
-                }
+            long sum = 0;
+            for(int i=0; i<b.length; ++i){
+                sum += Math.min(b[i], mid);
             }
-            if (met < n) {
+            if (sum < n*mid) {
                 u = mid - 1;
             } else {
                 l = mid + 1;
