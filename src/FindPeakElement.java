7f1a804fa446b441eb5db8236c825857680df7b5
flyingpenguinn <zjm84812@gmail.com>
2023-08-17 00:00:47 -0400

    tt

--- src/FindPeakElement.java
+++ src/FindPeakElement.java
@@ -2,21 +2,23 @@ public class FindPeakElement {
     public int findPeakElement(int[] a) {
         int n = a.length;
         int l = 0;
-        int u = n-1;
-        long inf = Integer.MIN_VALUE - 1L;
-        while(l<=u){
-            int mid = l+(u-l)/2;
-            long left = mid==0 ? inf : a[mid-1];
-            long right = mid==n-1? inf : a[mid+1];
+        int u = n - 1;
+        while (l <= u) {
+            int mid = l + (u - l) / 2;
+            if (mid == 0) {
+                // can't go more left
+                l = mid + 1;
+                continue;
+            }
+            int left = mid - 1;
             long cur = a[mid];
-            if(cur>left && cur>right){
-                return mid;
-            }else if (cur>left &&  cur<right){
-                l = mid+1;
-            }else{
-                u = mid-1;
+            long cleft = a[left];
+            if (cur >= cleft) {
+                l = mid + 1;
+            } else {
+                u = mid - 1;
             }
         }
-        return -1;
+        return u;
     }
 }
