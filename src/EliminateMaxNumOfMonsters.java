c745d29522bb7da8be66ecb4bbebaeeb95e6fba9
flyingpenguinn <zjm84812@gmail.com>
2023-11-06 21:31:57 -0500

    Update EliminateMaxNumOfMonsters.java

--- src/EliminateMaxNumOfMonsters.java
+++ src/EliminateMaxNumOfMonsters.java
@@ -4,15 +4,16 @@ public class EliminateMaxNumOfMonsters {
     // really confused by "eliminate any monster" during the contest...it means we can choose any one of them but may not be the front one
     public int eliminateMaximum(int[] dist, int[] speed) {
         int n = dist.length;
-        int[][] m = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            m[i][0] = dist[i];
-            m[i][1] = speed[i];
-            m[i][2] = (int) Math.ceil(dist[i] * 1.0 / speed[i]);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int d = dist[i];
+            int s = speed[i];
+            int t = (int) Math.ceil(d * 1.0 / s);
+            arr[i] = t;
         }
-        Arrays.sort(m, (x, y) -> Integer.compare(x[2], y[2]));
-        for (int i = 0; i < n; i++) {
-            if (m[i][1] * i >= m[i][0]) {
+        Arrays.sort(arr);
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] <= i) {
                 return i;
             }
         }
