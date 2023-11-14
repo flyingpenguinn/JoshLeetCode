c183342be5ead62f70cd38908becb74fd9195ac1
flyingpenguinn <zjm84812@gmail.com>
2023-11-13 21:37:25 -0500

    222

--- src/DistributeCandiesAmongChildrenIandIIandIII.java
+++ src/DistributeCandiesAmongChildrenIandIIandIII.java
@@ -1,57 +1,33 @@
 public class DistributeCandiesAmongChildrenIandIIandIII {
-    // result oriented programming...
-    public long distributeCandies(int n, int limit) {
-        if (n < limit) {
-            long start = 1;
-            long end = n + 1;
-            long res = (start + end) * (end - start + 1) / 2;
-            return res;
-        } else if (n >= limit && n <= 2 * limit) {
-            long peak = limit + 1;
-            long start1 = peak - (n - limit);
-            long seg1 = peak - start1 + 1;
-            long sum1 = (start1 + peak) * seg1 / 2;
-            long seg2 = peak - seg1;
-            long start2 = peak - seg2;
-            long sum2 = (start2 + peak - 1) * seg2 / 2;
-            return sum1 + sum2;
-        } else if (n <= 3 * limit) {
-            long diff = n - 2 * limit;
-            long start = 1;
-            long end = limit + 1 - diff;
-            long res = (start + end) * (end - start + 1) / 2;
-            return res;
-        } else {
+    // inclusion/ exclusion
+    private long countAll(long n) {
+        if(n<0){
             return 0;
         }
+        // stars and bars, n stars, 2 bars, so c(n+2, 2) ways to put bars
+        long allCases = comb(n + 2);
+        return allCases;
     }
 
-    public long distributeCandiesSlow(int n, int limit) {
-        return solve(n, 3, limit);
+    public long distributeCandies(int n, int limit) {
+        long allCases = countAll(n);
+
+        // we give one of the child limit first, then apply stars and bars
+        long bad = limit+1;
+        long oneOrMore = 3 * countAll(n - bad);
+        long twoOrMore = 3 * countAll(n - 2 * bad);
+        long threeOrMore = countAll(n - 3 * bad);
+        long res = allCases - oneOrMore + twoOrMore - threeOrMore;
+        return res;
     }
 
-    private long solve(long n, long rem, long p) {
-        if (rem == 2) {
-            if (n - p > p) {
-                return 0;
-            }
-            long start = Math.max(n - p, 0);
-            long end = Math.min(p, n);
-            long res = end - start + 1;
-            return res;
-        }
-        long res = 0;
-        for (long i = 0; i <= p && n - i >= 0; ++i) {
-            long cur = solve(n - i, rem - 1, p);
-            System.out.println((n - i) + " " + cur);
-            res += cur;
-        }
+    private long comb(long n) {
+        long res = n*(n-1)/2;
         return res;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(new DistributeCandiesAmongChildrenIandIIandIII().distributeCandies(18, 6));
-        System.out.println(new DistributeCandiesAmongChildrenIandIIandIII().distributeCandiesSlow(19, 6));
+        System.out.println(new DistributeCandiesAmongChildrenIandIIandIII().distributeCandies(5, 2));
     }
 }
