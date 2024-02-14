10f4af8ef773200842d185ced469aed56112e2e0
flyingpenguinn <zjm84812@gmail.com>
2024-02-13 19:20:01 -0500

    Update FirstDayWhereYouHavebeenInAllTheRooms.java

--- src/FirstDayWhereYouHavebeenInAllTheRooms.java
+++ src/FirstDayWhereYouHavebeenInAllTheRooms.java
@@ -1,15 +1,23 @@
 public class FirstDayWhereYouHavebeenInAllTheRooms {
     // must go from i-1 to i. when we first reach i-1, we need to go back to a[i-1] then go back to i-1, this time going to i
-    private int Mod = (int)(1e9+7);
+    private int mod = (int) (1e9 + 7);
+
     public int firstDayBeenInAllRooms(int[] a) {
         int n = a.length;
-        long[] dp =new long[n];
-        for(int i=1; i<n; ++i){
-            dp[i] = (dp[i-1]+dp[i-1]-dp[a[i-1]] +2) %Mod;
-            if(dp[i]<0){
-                dp[i] += Mod;
+        long[] psum = new long[n];
+        for (int i = 0; i < n - 1; ++i) {
+            long cur = 2;
+            if (a[i] < i) {
+                int pre = a[i];
+                cur += (psum[i - 1] - (pre == 0 ? 0 : psum[pre - 1])); // rego from pre to i-1 just one more time
+                cur %= mod;
+                if (cur < 0) {
+                    cur += mod;
+                }
             }
+            psum[i] = (i == 0 ? 0 : psum[i - 1]) + cur;
+            psum[i] %= mod;
         }
-        return (int)dp[n-1];
+        return (int) psum[n - 2];
     }
 }
