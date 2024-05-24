4b4dee262bb378586f4ab7f51ca6853485a36b2f
flyingpenguinn <zjm84812@gmail.com>
2024-05-24 00:10:33 -0400

    Update NumberOfBeautifulSubsets.java

--- src/NumberOfBeautifulSubsets.java
+++ src/NumberOfBeautifulSubsets.java
@@ -1,28 +1,59 @@
+import base.ArrayUtils;
+
+import java.util.*;
+
 public class NumberOfBeautifulSubsets {
-    public int beautifulSubsets(int[] a, int k) {
+    public int beautifulSubsets(int[] a, int ik) {
         int n = a.length;
-        int res = 0;
-        for (int st = 1; st < (1 << n); ++st) {
-            boolean[] set = new boolean[1001];
-            boolean bad = false;
-            for (int i = 0; i < n; ++i) {
-                if (((st >> i) & 1) == 1) {
-                    int cur = a[i];
-                    if (cur - k >= 1 && set[cur - k]) {
-                        bad = true;
-                        break;
-                    }
-                    if (cur + k <= 1000 && set[cur + k]) {
-                        bad = true;
-                        break;
-                    }
-                    set[cur] = true;
-                }
-            }
-            if (!bad) {
-                ++res;
-            }
+        this.dist = ik;
+        Map<Integer, List<Integer>> m = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            m.computeIfAbsent(a[i] % dist, k -> new ArrayList<>()).add(a[i]);
         }
+        int res = 1;
+        for (int k : m.keySet()) {
+            List<Integer> list = m.get(k);
+            Collections.sort(list);
+            dp = new int[list.size()];
+            Arrays.fill(dp, -1);
+            int ways = solve(list, 0);
+            res *= ways;
+        }
+        return res - 1;
+    }
+
+    private int dist = 0;
+    private int[] dp;
+
+    // like house robber, but in groups sense
+    // for each number check the same values and pick a subset from them
+    private int solve(List<Integer> list, int i) {
+        int n = list.size();
+        if (i == n) {
+            return 1;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int k = i;
+        while (k < n && list.get(k) == list.get(i)) {
+            ++k;
+        }
+        int way1 = solve(list, k);
+        int len = k - i;
+        int j = k;
+        while (j < n && list.get(j) == list.get(i) + dist) {
+            ++j;
+        }
+        int way2 = ((1 << len) - 1) * solve(list, j);
+        System.out.println(i + " " + way1 + " " + way2);
+        int res = way1 + way2;
+        dp[i] = res;
         return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new NumberOfBeautifulSubsets().beautifulSubsets(ArrayUtils.read1d("1,1,2"), 1));
     }
 }
