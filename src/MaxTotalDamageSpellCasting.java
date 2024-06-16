72586c4c2feb6519a19aebc8f67a2bf454ace401
flyingpenguinn <zjm84812@gmail.com>
2024-06-16 17:25:56 -0400

    wk

--- src/MaxTotalDamageSpellCasting.java
+++ src/MaxTotalDamageSpellCasting.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
 
 public class MaxTotalDamageSpellCasting {
+    // if written in bottom up way then we dont need the binary search can have a j as "catch up"
     public long maximumTotalDamage(int[] a) {
         int n = a.length;
         dp = new long[n];
         Arrays.fill(dp, -1L);
-        npos = new int[n];
-        Arrays.fill(npos, -2);
         Arrays.sort(a);
         return solve(a, 0);
     }
 
-    private int[] npos;
     private long[] dp;
 
     private long solve(int[] a, int i){
@@ -29,14 +27,8 @@ public class MaxTotalDamageSpellCasting {
         long len = k-i;
         long way1 = solve(a, k);
         long way2 = 0;
-        int j = -1;
-        // first >= this num
-        if(npos[i] != -2){
-            j = npos[i];
-        }else{
-            j = binary(a, k, n-1, a[i]+3);
-            npos[i] = j;
-        }
+        int j = binary(a, k, n-1, a[i]+3);
+
         way2 = len*a[i] + solve(a, j);
         long res = Math.max(way1, way2);
         dp[i] = res;
