8182a5d1a24496df098938bee6d36a03b5498442
flyingpenguinn <zjm84812@gmail.com>
2024-06-19 20:49:49 -0400

    Update MinMovesPeacefulBoard.java

--- src/MinMovesPeacefulBoard.java
+++ src/MinMovesPeacefulBoard.java
@@ -4,7 +4,40 @@ import java.util.List;
 
 public class MinMovesPeacefulBoard {
     // decompose row and column!
-    // then can also sort rows or columns and assign first row to row1, 2nd row to row 2, and so forth
+    // then imilar to distribute coins in binary tree: for each pos move = abs(what we have so far - what is really needed) then add them together
+    public int minMoves(int[][] a) {
+        int n = a.length;
+        int[] r = new int[n];
+        int[] c = new int[n];
+        for (int[] ai : a) {
+            ++r[ai[0]];
+            ++c[ai[1]];
+        }
+        ////   System.out.println(Arrays.toString(r));
+        //   System.out.println(Arrays.toString(c));
+        int rr = solve(r);
+        int cr = solve(c);
+        //  System.out.println(rr+" "+cr);
+        return rr + cr;
+    }
+
+    private int solve(int[] a) {
+        int n = a.length;
+        int res = 0;
+        int have = 0;
+        for (int i = 0; i < n; ++i) {
+            have += a[i];
+            int delta = Math.abs(have - (i + 1));
+            res += delta;
+        }
+        return res;
+    }
+
+}
+
+
+class MinMovesPeacefulBoardSorting {
+    // another way based on sorting
     public int minMoves(int[][] a) {
         int n = a.length;
         int res = 0;
