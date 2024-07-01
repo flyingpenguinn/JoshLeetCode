d9ef7b380e090813e3e282ee16e483354b60cfb0
flyingpenguinn <zjm84812@gmail.com>
2024-06-30 22:09:37 -0400

    Update FindMaxDiamaeterAfterMergingTwoTrees.java

--- src/FindMaxDiamaeterAfterMergingTwoTrees.java
+++ src/FindMaxDiamaeterAfterMergingTwoTrees.java
@@ -1,65 +1,76 @@
+import base.ArrayUtils;
+
 import java.util.*;
 
 public class FindMaxDiamaeterAfterMergingTwoTrees {
     // get diameter of either tree then find the best merge
-    private List<Set<Integer>> tree1, tree2;
-    private int[] dist1, dist2;
-    private int n, m;
+    private Set<Integer>[] t1;
+    private Set<Integer>[] t2;
 
     public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
-        this.n = edges1.length + 1;
-        this.m = edges2.length + 1;
-        tree1 = new ArrayList<>();
-        tree2 = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            tree1.add(new HashSet<>());
+        int en1 = edges1.length;
+        int n1 = en1 + 1;
+        int en2 = edges2.length;
+        int n2 = en2 + 1;
+        t1 = new HashSet[n1];
+        t2 = new HashSet[n2];
+        for (int i = 0; i < n1; ++i) {
+            t1[i] = new HashSet<>();
         }
-
-        for (int i = 0; i < m; i++) {
-            tree2.add(new HashSet<>());
+        for (int i = 0; i < n2; ++i) {
+            t2[i] = new HashSet<>();
         }
-
-        for (int[] edge : edges1) {
-            tree1.get(edge[0]).add(edge[1]);
-            tree1.get(edge[1]).add(edge[0]);
+        for (int[] e1 : edges1) {
+            int u = e1[0];
+            int v = e1[1];
+            t1[u].add(v);
+            t1[v].add(u);
         }
-
-        for (int[] edge : edges2) {
-            tree2.get(edge[0]).add(edge[1]);
-            tree2.get(edge[1]).add(edge[0]);
+        for (int[] e2 : edges2) {
+            int u = e2[0];
+            int v = e2[1];
+            t2[u].add(v);
+            t2[v].add(u);
         }
-
-        int diameter1 = getDiameter(tree1, n);
-        int diameter2 = getDiameter(tree2, m);
-
-        return Math.max(diameter1, Math.max(diameter2, (diameter1 + 1) / 2 + (diameter2 + 1) / 2 + 1));
+        int diag1 = getDiameter(t1);
+        int diag2 = getDiameter(t2);
+        int l1 = (diag1 + 1) / 2;
+        int l2 = (diag2 + 1) / 2;
+        return Math.max(diag1, Math.max(diag2, l1 + l2 + 1));
     }
 
-    private int getDiameter(List<Set<Integer>> tree, int n) {
-        dist1 = new int[n];
-        int farthest = bfs(tree, 0, dist1);
-        dist2 = new int[n];
-        farthest = bfs(tree, farthest, dist2);
-        return dist2[farthest];
+    private int getDiameter(Set<Integer>[] t1) {
+        int[] res = bfs(t1, 0); // dist, node
+        int v2 = res[1];
+        res = bfs(t1, v2);
+        int rt = res[0];
+     //   System.out.println(v2 + " dist from 0 " + res[0]);
+    //    System.out.println(res[1] + " dist from  " + v2 + " " + res[0]);
+        return rt;
     }
 
-    private int bfs(List<Set<Integer>> tree, int start, int[] dist) {
-        Arrays.fill(dist, (int) 1e9);
-        dist[start] = 0;
-        Deque<Integer> queue = new ArrayDeque<>();
-        queue.add(start);
-        int res = start;
-        while (!queue.isEmpty()) {
-            int top = queue.poll();
-            int nd = dist[top] + 1;
-            for (int ne : tree.get(top)) {
+    private int Max = (int) (1e9);
+
+    private int[] bfs(Set<Integer>[] t, int v) {
+        int n = t.length;
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offerLast(v);
+        int[] dist = new int[n];
+        Arrays.fill(dist, Max);
+        dist[v] = 0;
+        int[] res = new int[]{0, 0};
+        while (!q.isEmpty()) {
+            int top = q.pollFirst();
+            int cd = dist[top];
+            System.out.println(top + " dist is " + cd);
+            int nd = cd + 1;
+            res[0] = cd;
+            res[1] = top;
+            for (int ne : t[top]) {
                 if (dist[ne] > nd) {
+               //     System.out.println("Pushing " + ne + " from " + top + " dist is " + nd);
                     dist[ne] = nd;
-                    queue.add(ne);
-                    if (dist[ne] > dist[res]) {
-                        res = ne;
-                    }
+                    q.offerLast(ne);
                 }
             }
         }
