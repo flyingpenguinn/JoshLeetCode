6ee3366f509232578a55f61aa4cd2269dcdebede
flyingpenguinn <zjm84812@gmail.com>
2024-07-08 22:58:13 -0400

    Update MinCostToMakeAllCharsEqual.java

--- src/MinCostToMakeAllCharsEqual.java
+++ src/MinCostToMakeAllCharsEqual.java
@@ -1,14 +1,34 @@
 public class MinCostToMakeAllCharsEqual {
     // couldnt figure out in the contest...
+    // actually just 2nd half go from middle to end, first half go from middle to beginning...
     public long minimumCost(String s) {
-        char[] sc = s.toCharArray();
+        return Math.min(solve(s, 1), solve(s, 0));
+    }
+
+    private long solve(String s, int t) {
         int n = s.length();
+        int ops = 0;
         long res = 0;
-        for (int i = 0; i + 1 < n; ++i) {
-            if (sc[i] != sc[i + 1]) {
-                res += Math.min(i + 1, n - i - 1);
+        for (int i = n / 2 - 1; i >= 0; --i) {
+            int v = s.charAt(i) - '0';
+            if ((v ^ ops) == t) {
+                continue;
+            } else {
+                res += i + 1;
+                ops ^= 1;
+            }
+        }
+        ops = 0;
+        for (int i = n / 2; i < n; ++i) {
+            int v = s.charAt(i) - '0';
+            if ((v ^ ops) == t) {
+                continue;
+            } else {
+                res += n - i;
+                ops ^= 1;
             }
         }
         return res;
     }
 }
+
