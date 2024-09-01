f795e972ecf2f14e593f8c43ec3ba309e6166fae
flyingpenguinn <zjm84812@gmail.com>
2024-09-01 11:34:16 -0400

    Update MaxXorScoreSubarrayQuery.java

--- src/MaxXorScoreSubarrayQuery.java
+++ src/MaxXorScoreSubarrayQuery.java
@@ -1,3 +1,7 @@
+import base.ArrayUtils;
+
+import java.util.Arrays;
+
 public class MaxXorScoreSubarrayQuery {
     public int[] maximumSubarrayXor(int[] a, int[][] qs) {
         int n = a.length;
@@ -6,24 +10,30 @@ public class MaxXorScoreSubarrayQuery {
         for (int i = 0; i < n; i++) {
             dp[i][i] = a[i];
         }
-
-        for (int len = 1; len < n; len++) {
-            for (int i = 0; i + len < n; i++) {
-                dp[i][i + len] = dp[i][i + len - 1] ^ dp[i + 1][i + len];
+        for(int len = 2; len<=n; ++len){
+            for(int i=0; i<n && i+len-1<n; ++i){
+                int j = i+len-1;
+                dp[i][j] = dp[i][j-1] ^ dp[i+1][j];
             }
         }
-        for (int j = 1; j < n; j++) {
-            for (int i = 0; i + j < n; i++) {
-                dp[i][i + j] = Math.max(dp[i][i + j],
-                        Math.max(dp[i][i + j - 1], dp[i + 1][i + j]));
+        for(int len = 1; len<=n; ++len){
+            for(int i=0; i<n && i+len-1<n; ++i){
+                int j = i+len-1;
+                dp[i][j] = Math.max(dp[i][j], Math.max((j==0?0:dp[i][j-1]), (i+1==n? 0: dp[i+1][j])));
             }
         }
-
         int[] res = new int[qs.length];
-        int idx = 0;
-        for (int[] q : qs) {
-            res[idx++] = dp[q[0]][q[1]];
+        for(int i = 0; i<qs.length; ++i){
+            int[] q = qs[i];
+            int l = q[0];
+            int r = q[1];
+            res[i] = dp[l][r];
         }
         return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new MaxXorScoreSubarrayQuery().maximumSubarrayXor(ArrayUtils.read1d("[2,8,4,32,16,1]"), ArrayUtils.read("[[0,2],[1,4],[0,5]]"))));
     }
 }
