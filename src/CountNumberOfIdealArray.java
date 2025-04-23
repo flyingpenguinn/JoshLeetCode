58867492782b112c5908077e3808bc189fbc11c2
flyingpenguinn <zjm84812@gmail.com>
2025-04-22 20:41:40 -0400

    www

--- src/CountNumberOfIdealArray.java
+++ src/CountNumberOfIdealArray.java
@@ -5,64 +5,113 @@ import java.util.List;
 import java.util.Map;
 
 public class CountNumberOfIdealArray {
-    // if strictly increase length <=14
-    // then use some combinatoric magic
-    //TODO do it myself
-    public int idealArrays(int n, int maxValue) {
-        int m = 1000000007;
-        BigInteger res = BigInteger.ZERO;
-        long[][] dp = new long[15][maxValue + 1];
-        Map<Integer, List<Integer>> map = buildMap(maxValue);
+    // count strictly increasing seq. then use stars and bars argument to "distribute" all n values onto len parts, where len is at most 14
+    private List<Integer>[] m;
+    private long mod = (long) (1e9 + 7);
 
-        // step 1: compute dp for the alternative problem (strictly increasing case)
-        for (int i = 1; i <= maxValue; i++) {
-            dp[1][i] = 1;
+    void factor(int lim) {
+        for (int i = 1; i <= lim; ++i) {
+            for (int j = 2 * i; j <= lim; j += i) {
+                m[j].add(i);
+            }
         }
-        for (int i = 2; i <= n && i <= 14; i++) {
-            for (int j = 1; j <= maxValue; j++) {
-                for (int k : map.get(j)) { 
-                    dp[i][j] += dp[i - 1][k];
-                    dp[i][j] %= m;
+    }
+
+    public int idealArrays(int n, int lim) {
+        fact = new Long[n + 1];
+        m = new ArrayList[lim+1];
+        for (int i = 1; i <= lim; ++i) {
+            m[i] = new ArrayList<>();
+        }
+        factor(lim);
+        int maxIncreasing = 14;
+        long[][] dp = new long[maxIncreasing + 1][lim + 1];
+
+        for (int j = 1; j <= lim; ++j) {
+            dp[1][j] = 1;
+        }
+        for (int i = 2; i <= maxIncreasing; ++i) {
+            for (int j = 1; j <= lim; ++j) {
+                for (int d : m[j]) {
+                    dp[i][j] += dp[i - 1][d] ;
                 }
             }
         }
-        for (int i = 1; i <= n && i <= 14; i++) {
-            for (int j = 1; j <= maxValue; j++) {
-                dp[i][0] += dp[i][j];
-                dp[i][0] %= m; // dp[i][0] = number of ideal arrays (strictly increasing case) of length i
+        long[] lens = new long[maxIncreasing + 1];
+        for (int i = 1; i <= maxIncreasing; ++i) {
+            for (int j = 1; j <= lim; ++j) {
+                lens[i] += dp[i][j];
             }
         }
-
-        // step 2: use combinatorics to get the final answer for the actual problem from the alternative problem (strictly increasing case)
-        for (int i = 1; i <= n && i <= 14; i++) {
-            res = res.add(nCk(n - 1, i - 1).multiply(BigInteger.valueOf(dp[i][0])));
-            res = res.mod(BigInteger.valueOf(m));
+        long res = 0;
+        for (int i = 1; i <= maxIncreasing; ++i) {
+            long cnk = combi(n - 1, i - 1);
+            long cur = cnk * lens[i];
+            cur %= mod;
+            res += cur;
+            res %= mod;
         }
-        return res.intValue();
+        return (int) res;
+
     }
 
-    // helper function to compute "n choose k"
-    private BigInteger nCk(int n, int k) {
-        BigInteger res = BigInteger.ONE;
-        for (int i = 1; i <= k; i++) {
-            res = res.multiply(BigInteger.valueOf(n - (i - 1))).divide(BigInteger.valueOf(i));
+    private Long[] fact;
+
+
+    private long fact(int n) {
+        if (fact[n] != null) {
+            return fact[n];
         }
+        long res = 0;
+        if (n == 0) {
+            res = 1L;
+        } else {
+            res = fact(n - 1) * n;
+            res %= mod;
+        }
+        fact[n] = res;
         return res;
     }
 
-    // helper funciton to build map {Integer -> {its divisors}}
-    private Map<Integer, List<Integer>> buildMap(int maxValue) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 1; i <= maxValue; i++) {
-            map.put(i, new ArrayList<>());
+    private long combi(int n, int k) {
+        if (n < k) {
+            return 0;
         }
-        for (int i = 1; i <= maxValue; i++) {
-            int j = i * 2; // strictly increasing
-            while (j <= maxValue) {
-                map.get(j).add(i);
-                j += i;
-            }
+        if (n == k) {
+            return 1;
         }
-        return map;
+        long res = fact(n);
+        res *= modinverse(fact(k), mod);
+        res %= mod;
+        res *= modinverse(fact(n - k), mod);
+        res %= mod;
+        return res;
+    }
+
+
+    private long modinverse(long a, long m) {
+        long m0 = m;
+        long y = 0, x = 1;
+        if (m == 1) {
+            return 0;
+        }
+        while (a > 1) {
+            long q = a / m;
+            long t = m;
+            m = a % m;
+            a = t;
+            t = y;
+            y = x - q * y;
+            x = t;
+        }
+        if (x < 0) {
+            x += m0;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CountNumberOfIdealArray().idealArrays(2, 5));
+        System.out.println(new CountNumberOfIdealArray().idealArrays(5,3));
     }
 }
