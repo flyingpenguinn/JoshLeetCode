c47be5842fd3197fd198a67e47a0572d94870e00
flyingpenguinn <zjm84812@gmail.com>
2025-05-18 01:09:41 -0400

    Update MinEdgeWeightEqualQueriesInTree.java

--- src/MinEdgeWeightEqualQueriesInTree.java
+++ src/MinEdgeWeightEqualQueriesInTree.java
@@ -1,4 +1,7 @@
+import base.ArrayUtils;
+
 import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class MinEdgeWeightEqualQueriesInTree {
@@ -9,97 +12,105 @@ And then for each query (x, y),
 The number of edges of a particular weight = num[x] + num[y] - 2 * num[LCA(x, y)].
 
      */
-    public int[] minOperationsQueries(int n, int[][] edges, int[][] queries) {
-        List<Pair>[] con = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            con[i] = new ArrayList<>();
+    private Map<Integer, Integer>[] t;
+    private int[][] lift;
+    private int[] level;
+    private int[][] cnt;
+    private int BITS = 18;
+    private int WCNT = 27;
+
+    private int getlca(int v1, int v2) {
+        if (level[v1] < level[v2]) {
+            int tmp = v1;
+            v1 = v2;
+            v2 = tmp;
         }
-
-        for (int[] e : edges) {
-            con[e[0]].add(new Pair(e[1], e[2] - 1));
-            con[e[1]].add(new Pair(e[0], e[2] - 1));
-        }
-
-        int[][] all = new int[n][26];
-        int[][] f = new int[n][20];
-        int[] b = new int[n];
-        int[] e = new int[n];
-        int[] w = new int[26];
-
-        int[] t = {0};
-        dfs(con, 0, -1, b, e, f, t, all, w);
-        f[0][0] = 0;
-
-        for (int i = 1; i < 20; ++i) {
-            for (int j = 0; j < n; ++j) {
-                f[j][i] = f[f[j][i - 1]][i - 1];
+        int diff = level[v1] - level[v2];
+        for (int k = 0; k < BITS; ++k) {
+            if (((diff >> k) & 1) == 1) {
+                v1 = lift[k][v1];
             }
         }
+        if (v1 == v2) {
+            return v1;
+        }
+        for (int k = BITS - 1; k >= 0; --k) {
+            if (lift[k][v1] != lift[k][v2]) {
+                v1 = lift[k][v1];
+                v2 = lift[k][v2];
+            }
+        }
+        return lift[0][v1];
+    }
 
-        int[] result = new int[queries.length];
-        int index = 0;
-        for (int[] q : queries) {
-            if (q[0] == q[1]) {
-                result[index++] = 0;
+    public int[] minOperationsQueries(int n, int[][] edges, int[][] queries) {
+        t = new HashMap[n];
+        lift = new int[BITS][n];
+        level = new int[n];
+        cnt = new int[n][WCNT];
+        for (int i = 0; i < n; ++i) {
+            t[i] = new HashMap<Integer, Integer>();
+        }
+        for (int[] e : edges) {
+            int v1 = e[0];
+            int v2 = e[1];
+            int w = e[2];
+            t[v1].put(v2, w);
+            t[v2].put(v1, w);
+        }
+        int[] ccnt = new int[WCNT];
+        dfs(0, -1, 0, ccnt);
+        for (int k = 1; k < BITS; ++k) {
+            for (int i = 0; i < n; ++i) {
+                lift[k][i] = lift[k - 1][lift[k - 1][i]];
+            }
+        }
+        int qn = queries.length;
+        int[] res = new int[qn];
+        for (int i = 0; i < qn; ++i) {
+            int v1 = queries[i][0];
+            int v2 = queries[i][1];
+            int lca = getlca(v1, v2);
+            int[] cnt1 = cnt[v1];
+            int[] cnt2 = cnt[v2];
+            int[] cnt3 = cnt[lca];
+            int[] allcnt = new int[WCNT];
+            for (int k = 0; k < WCNT; ++k) {
+                allcnt[k] = cnt1[k] + cnt2[k] - 2 * cnt3[k];
+            }
+            int allsum = 0;
+            int maxocc = 0;
+            for (int k = 0; k < WCNT; ++k) {
+                if (allcnt[k] == 0) {
+                    continue;
+                }
+                allsum += allcnt[k];
+                maxocc = Math.max(maxocc, allcnt[k]);
+            }
+            int changes = allsum - maxocc;
+            res[i] = changes;
+        }
+        return res;
+    }
+
+    private void dfs(int i, int parent, int cl, int[] ccnt) {
+        level[i] = cl;
+        for (int k = 0; k < WCNT; ++k) {
+            cnt[i][k] = ccnt[k];
+        }
+        for (int ne : t[i].keySet()) {
+            if (parent == ne) {
                 continue;
             }
-            int tLca = lca(f, b, e, q[0], q[1]);
-            int sum = 0, m = 0;
-            for (int i = 0; i < 26; ++i) {
-                int temp = all[q[0]][i] + all[q[1]][i] - (all[tLca][i] << 1);
-                sum += temp;
-                m = Math.max(m, temp);
-            }
-            result[index++] = sum - m;
-        }
-        return result;
-    }
-
-    private static class Pair {
-        int first;
-        int second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+            int cw = t[i].get(ne);
+            ++ccnt[cw];
+            lift[0][ne] = i;
+            dfs(ne, i, cl + 1, ccnt);
+            --ccnt[cw];
         }
     }
 
-    void dfs(List<Pair>[] con, int x, int p, int[] b, int[] e, int[][] f, int[] t, int[][] all, int[] w) {
-        all[x] = w.clone();
-        f[x][0] = p;
-        b[x] = ++t[0];
-        for (Pair v : con[x]) {
-            if (v.first != p) {
-                w[v.second]++;
-                dfs(con, v.first, x, b, e, f, t, all, w);
-                w[v.second]--;
-            }
-        }
-        e[x] = t[0];
-    }
-
-    boolean isAncestor(int[] b, int[] e, int x, int y) {
-        return b[x] <= b[y] && e[x] >= e[y];
-    }
-
-    int lca(int[][] f, int[] b, int[] e, int x, int y) {
-        if (isAncestor(b, e, x, y)) {
-            return x;
-        }
-        if (isAncestor(b, e, y, x)) {
-            return y;
-        }
-
-        int r = 0;
-        for (int i = 19; i >= 0; --i) {
-            int temp = f[x][i];
-            if (isAncestor(b, e, temp, y)) {
-                r = temp;
-            } else {
-                x = temp;
-            }
-        }
-        return r;
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new MinEdgeWeightEqualQueriesInTree().minOperationsQueries(8, ArrayUtils.read("[[1,2,6],[1,3,4],[2,4,6],[2,5,3],[3,6,6],[3,0,8],[7,0,2]]"), ArrayUtils.read("[[4,6],[0,4],[6,5],[7,4]]"))));
     }
 }
