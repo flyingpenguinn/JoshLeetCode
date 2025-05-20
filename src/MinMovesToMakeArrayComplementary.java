7125c6881483e143f73c112071ca6fd863d85f93
flyingpenguinn <zjm84812@gmail.com>
2025-05-19 20:51:41 -0400

    www

--- src/MinMovesToMakeArrayComplementary.java
+++ src/MinMovesToMakeArrayComplementary.java
@@ -1,38 +1,54 @@
 import base.ArrayUtils;
 
+import java.util.*;
+
 import static java.lang.Math.min;
 import static java.lang.Math.max;
 
 public class MinMovesToMakeArrayComplementary {
-    public int minMoves(int[] a, int limit) {
+
+    // here we use line sweeping. can also use binary search to look for good Rs and minus bad Ls
+    public int minMoves(int[] a, int lim) {
         int n = a.length;
-        // if the target is from [2 to 1+ min), need 2 changes
-        // from 1 + min to sum), need 1 change
-        // sum, sum+1) 0 change
-        // [sum+1 to limit+max+1), need 1 change
-        // from limit+max+1 to 2*limit+1), need 2 changes
-        // use the res array to get a running total at each possible point of target sum
-        int[] res = new int[2 * limit + 2];
-        // from 2 to 2*limit
-        for (int i = 0; i < n / 2; i++) {
-            int sum = a[i] + a[n - i - 1];
-            int min = Math.min(a[i], a[n - i - 1]);
-            int max = Math.max(a[i], a[n - i - 1]);
-            res[2] += 2;
-            res[1 + min] -= 1;
-            res[sum] -= 1;
-            res[sum + 1] += 1;
-            res[limit + max + 1] += 1;
-            res[2 * limit + 1] -= 2;
+        int pairs = n / 2;
+        List<Event> events = new ArrayList<>();
+        Map<Integer, Integer> hits = new HashMap<>();
+        for (int i = 0; i < pairs; i++) {
+            int v1 = a[i], v2 = a[n - 1 - i];
+            int minv = Math.min(v1, v2), maxv = Math.max(v1, v2);
+            int L = 1 + minv, R = lim + maxv;
+            events.add(new Event(L, 1));
+            events.add(new Event(R + 1, -1));
+            hits.put(v1 + v2, hits.getOrDefault(v1 + v2, 0) + 1);
         }
-        int rt = 0;
-        int min = 2 * n + 1;
-        for (int i = 2; i <= 2 * limit; i++) {
-            rt += res[i];
-            min = Math.min(min, rt);
+        Set<Integer> posSet = new HashSet<>();
+        for (Event e : events) posSet.add(e.pos);
+        posSet.addAll(hits.keySet());
+        List<Integer> posList = new ArrayList<>(posSet);
+        Collections.sort(posList);
+        events.sort(Comparator.comparingInt(e -> e.pos));
+        int idx = 0, active = 0, best = Integer.MAX_VALUE, base = 2 * pairs;
+        for (int pos : posList) {
+            while (idx < events.size() && events.get(idx).pos == pos) {
+                active += events.get(idx).delta;
+                idx++;
+            }
+            int hitCount = hits.getOrDefault(pos, 0);
+            best = Math.min(best, base - active - hitCount);
         }
-        return min;
+        return best;
     }
+
+    private static class Event {
+        int pos, delta;
+
+        Event(int p, int d) {
+            pos = p;
+            delta = d;
+        }
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println(new MinMovesToMakeArrayComplementary().minMoves(ArrayUtils.read1d("[3,1,1,1,2,3,2,3,1,3,2,1]"), 3));
