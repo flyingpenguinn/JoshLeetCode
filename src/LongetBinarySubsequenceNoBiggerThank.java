98f8724f5a839a56652b84f49a6ca655505524e0
flyingpenguinn <zjm84812@gmail.com>
2025-06-25 22:13:16 -0400

    Update LongetBinarySubsequenceNoBiggerThank.java

--- src/LongetBinarySubsequenceNoBiggerThank.java
+++ src/LongetBinarySubsequenceNoBiggerThank.java
@@ -1,56 +1,19 @@
 public class LongetBinarySubsequenceNoBiggerThank {
-    // TODO greedy may work as well
-    private Integer[][][][] dp;
     public int longestSubsequence(String s, int k) {
-        String binary = Integer.toBinaryString(k);
-        dp = new Integer[s.length()][binary.length()][2][2];
-        return solve(s, 0, binary, 0, 0, 0);
-    }
-
-    // l: do we have the freedom to be 1 or 0
-    // k: 1 appeared or not
-    private int solve(String s, int i, String t, int j, int k, int l) {
-        if(i==s.length()){
-            return 0;
-        }
-        if(j==t.length()){
-            return 0;
-        }
-        if(s.length()-i < t.length()-j){
-            return s.length()-i;
-        }
-        if(dp[i][j][k][l] != null){
-            return dp[i][j][k][l];
-        }
-        int res = solve(s, i+1, t, j, k, l);
-        if(l==1){
-            res = Math.max(1+solve(s, i+1, t, j+1, k, l), res);
-        }else{
-            char sc = s.charAt(i);
-            if(sc=='0'){
-                if(k==0){
-                    res = Math.max(1+solve(s, i+1, t, j, k, l), res);
-                }else{
-                    char tc = t.charAt(j);
-                    if(tc=='1'){
-                        res = Math.max(1+solve(s, i+1, t, j+1, k, 1), res);
-                    }else{
-                        res = Math.max(1+solve(s, i+1, t, j+1, k, 0), res);
-                    }
-                }
+        int n = s.length();
+        long cur = 0;
+        int res = 0;
+        for(int i=n-1; i>=0; --i){
+            int v = s.charAt(i)-'0';
+            if(v==0){
+                ++res;
             }else{
-
-                if(k==0){
-                    res= Math.max(1+solve(s, i+1, t, j+1, 1, 0), res);
-                }else{
-                    char tc = t.charAt(j);
-                    if(tc=='1'){
-                        res = Math.max(1+solve(s, i+1, t, j+1, k, l), res);
-                    }
+                if(res<=31 && cur + (1L<<res) <=k){
+                    cur += (1<<res);
+                    ++res;
                 }
             }
         }
-        dp[i][j][k][l] = res;
         return res;
     }
 
