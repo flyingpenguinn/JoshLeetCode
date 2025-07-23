589831314e9e0bb0c2301bf9f0f2988881cb9ce7
flyingpenguinn <zjm84812@gmail.com>
2025-07-22 22:16:09 -0400

    Update MaxHeightByStackingCuboids.java

--- src/MaxHeightByStackingCuboids.java
+++ src/MaxHeightByStackingCuboids.java
@@ -12,33 +12,62 @@ public class MaxHeightByStackingCuboids {
     // a2 >=b3>=b2
     // a3 >=a2>=b3
     // hence we can rotate and make the whole thing taller
+
+    // 2D BIT
+    int[][] bit;
+
+    int q(int y, int z) {
+        int res = 0;
+        int oz = z;
+        while (y > 0) {
+            z = oz;
+            while (z > 0) {
+                res = Math.max(res, bit[y][z]);
+                z -= (z & -z);
+            }
+            y -= (y & -y);
+        }
+        return res;
+    }
+
+    void u(int y, int z, int v) {
+        int yn = bit.length;
+        int zn = bit[0].length;
+        int oz = z;
+        while (y < yn) {
+            z = oz;
+            while (z < zn) {
+                bit[y][z] = Math.max(bit[y][z], v);
+                z += z & -z;
+            }
+            y += y & -y;
+        }
+    }
+
     public int maxHeight(int[][] a) {
         int n = a.length;
-        for(int i=0; i<n; i++){
+        bit = new int[105][105];
+        for (int i = 0; i < n; ++i) {
             Arrays.sort(a[i]);
         }
-        // must sort by all 3 dimensions. if we only sort by first dimension then order could be reversed when first dim is the same
-        Arrays.sort(a, (x,y)-> {
-            if(x[0]== y[0]){
-                if(x[1]!= y[1]){
-                    return Integer.compare(x[1], y[1]);
-                }else{
-                    return Integer.compare(x[2], y[2]);
-                }
-            }else{
+        Arrays.sort(a, (x, y) -> {
+            if (x[0] != y[0]) {
                 return Integer.compare(x[0], y[0]);
+            } else if (x[1] != y[1]) {
+                return Integer.compare(x[1], y[1]);
+            } else {
+                return x[2] - y[2];
             }
         });
-        int[] dp = new int[n];
         int res = 0;
-        for(int i=0; i<n; i++){
-            dp[i] = a[i][2];
-            for(int j=0; j<i; j++){
-                if(a[j][1] <= a[i][1] && a[j][2]<= a[i][2]){
-                    dp[i] = Math.max(dp[i], a[i][2]+dp[j]);
-                }
-            }
-            res = Math.max(res, dp[i]);
+        for (int i = 0; i < n; i++) {
+            int y = a[i][1];
+            int z = a[i][2];
+            int mb = q(y, z);
+            int cur = mb + z;
+            u(y, z, cur);
+            //  System.out.println(y+" "+z+" "+mb+" "+cur);
+            res = Math.max(res, cur);
         }
         return res;
     }
