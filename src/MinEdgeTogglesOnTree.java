edf2244f85be8cc73024f54cbc1a75926d9883cd
flyingpenguinn <zjm84812@gmail.com>
2026-01-19 19:11:07 -0500

    Update MinEdgeTogglesOnTree.java

--- src/MinEdgeTogglesOnTree.java
+++ src/MinEdgeTogglesOnTree.java
@@ -1,88 +1,58 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class MinEdgeTogglesOnTree {
-    // TODO
-    private int[] h, to, nx, id, d;
-    private boolean[] use;
-    private boolean ok;
+    private List<int[]>[] tree;
+    private int[] d;
+    private List<Integer> res = new ArrayList<>();
 
-    private int dfs(int u, int p, int pe) {
-        int x = 0;
-        for (int e = h[u]; e != -1; e = nx[e]) {
-            int v = to[e];
-            if (v == p) {
-                continue;
-            }
-            x ^= dfs(v, u, id[e]);
-            if (!ok) {
-                return 0;
-            }
+    public List<Integer> minimumFlips(int n, int[][] edges, String s, String t) {
+        d = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            int sind = s.charAt(i) - '0';
+            int tind = t.charAt(i) - '0';
+            d[i] = sind ^ tind;
         }
-        if (u == 0) {
-            if ((d[0] ^ x) != 0) {
-                ok = false;
-            }
-            return 0;
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; ++i) {
+            tree[i] = new ArrayList<>();
         }
-        int v = d[u] ^ x;
-        if (v == 1) {
-            use[pe] = true;
+        int en = edges.length;
+        for (int i = 0; i < en; ++i) {
+
+            int v1 = edges[i][0];
+            int v2 = edges[i][1];
+            tree[v1].add(new int[]{v2, i});
+            tree[v2].add(new int[]{v1, i});
         }
-        return v;
+        int flip = dfs(0, -1);
+        if (flip == 1) {
+            return List.of(-1);
+        }
+        Collections.sort(res);
+        return res;
     }
 
-    public List<Integer> minimumFlips(int n, int[][] edges, String start, String target) {
-        d = new int[n];
-        int xo = 0;
-        for (int i = 0; i < n; ++i) {
-            d[i] = (start.charAt(i) - '0') ^ (target.charAt(i) - '0');
-            xo ^= d[i];
-        }
-        if (xo != 0) {
-            return Collections.singletonList(-1);
-        }
-
-        int m = n - 1;
-        h = new int[n];
-        Arrays.fill(h, -1);
-        to = new int[m * 2];
-        nx = new int[m * 2];
-        id = new int[m * 2];
-
-        int it = 0;
-        for (int i = 0; i < m; ++i) {
-            int a = edges[i][0], b = edges[i][1];
-
-            to[it] = b;
-            id[it] = i;
-            nx[it] = h[a];
-            h[a] = it;
-            it++;
-
-            to[it] = a;
-            id[it] = i;
-            nx[it] = h[b];
-            h[b] = it;
-            it++;
-        }
-
-        use = new boolean[m];
-        ok = true;
-        dfs(0, -1, -1);
-
-        if (!ok) {
-            return Collections.singletonList(-1);
-        }
-
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = 0; i < m; ++i) {
-            if (use[i]) {
-                res.add(i);
+    private int dfs(int i, int p) {
+        int xorall = 0;
+        for (int[] nes : tree[i]) {
+            int ne = nes[0];
+            if (ne == p) {
+                continue;
             }
+            int needhelp = dfs(ne, i);
+            if (needhelp == 1) {
+                res.add(nes[1]);
+                xorall ^= 1;
+            }
+
         }
-        return res;
+        if (xorall != d[i]) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
