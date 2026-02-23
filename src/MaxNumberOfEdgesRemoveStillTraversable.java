73fbd472a734464608ed1c5ce6798e1ffcfc6c57
flyingpenguinn <zjm84812@gmail.com>
2026-02-23 15:31:03 -0500

    Update MaxNumberOfEdgesRemoveStillTraversable.java

--- src/MaxNumberOfEdgesRemoveStillTraversable.java
+++ src/MaxNumberOfEdgesRemoveStillTraversable.java
@@ -1,72 +1,102 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class MaxNumberOfEdgesRemoveStillTraversable {
     // try to keep type 3 edges. calculate MST using 3 first then 1 and 2. those 1 and 2 are deletable
-    // note we need to use a set to filter duplicated edges like deltable 3
-    private Set<List<Integer>> removed = new HashSet<>();
+    // DSU UF template
+    private static class UF {
+        public int[] pa;
+        public int[] psize;
 
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
-        int[] pa = new int[n + 1];
+        public UF(int size) {
+            pa = new int[size];
+            psize = new int[size];
+            Arrays.fill(pa, -1);
+            Arrays.fill(psize, 1);
+        }
 
-        List<int[]> ae = new ArrayList<>();
-        List<int[]> be = new ArrayList<>();
-        for (int i = 0; i < edges.length; i++) {
-            if (edges[i][0] == 1) {
-                ae.add(edges[i]);
-            } else if (edges[i][0] == 2) {
-                be.add(edges[i]);
-            } else {
-                ae.add(edges[i]);
-                be.add(edges[i]);
+        public int find(int i) {
+            if (pa[i] == -1) {
+                return i;
             }
+            int rt = find(pa[i]);
+            pa[i] = rt;
+            return rt;
         }
-        int ra = count(ae, pa);
-        if (ra == -1) {
-            return -1;
+
+        public boolean union(int i, int j) {
+            int ai = find(i);
+            int aj = find(j);
+            if (ai == aj) {
+                return false;
+            }
+            if (psize[ai] < psize[aj]) {
+                pa[ai] = aj;
+                psize[aj] += psize[ai];
+            } else {
+                pa[aj] = ai;
+                psize[ai] += psize[aj];
+            }
+            return true;
         }
-        int rb = count(be, pa);
-        if (rb == -1) {
-            return -1;
-        }
-        return removed.size();
+
     }
 
-    private int count(List<int[]> es, int[] pa) {
-        // 3 first
-        Collections.sort(es, (x, y) -> Integer.compare(y[0], x[0]));
-
-        Arrays.fill(pa, -1);
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        UF ufa = new UF(n + 1);
+        UF ufb = new UF(n + 1);
         int res = 0;
-        for (int i = 0; i < es.size(); i++) {
-            int[] e = es.get(i);
-            int roota = find(e[1], pa);
-            int rootb = find(e[2], pa);
-            if (roota != rootb) {
-                pa[roota] = rootb;
-            } else {
-                res++;
-                removed.add(edge(e));
+        for (int[] e : edges) {
+            int type = e[0];
+            if (type != 3) {
+                continue;
+            }
+            int u = e[1];
+            int v = e[2];
+            boolean ra = ufa.union(u, v);
+            boolean rb = ufb.union(u, v);
+            if (!ra && !rb) {
+                ++res;
             }
         }
-        boolean seen = false;
-        for (int i = 1; i < pa.length; i++) {
-            if (pa[i] == -1) {
-                if (seen) {
-                    return -1;
-                }
-                seen = true;
+        for (int[] e : edges) {
+            int type = e[0];
+            if (type == 3) {
+                continue;
             }
+            int u = e[1];
+            int v = e[2];
+            if (type == 1) {
+                boolean ra = ufa.union(u, v);
+                if (!ra) {
+                    ++res;
+                }
+            } else {
+                boolean rb = ufb.union(u, v);
+                if (!rb) {
+                    ++res;
+                }
+            }
+        }
+        int roota = getRootCounts(ufa);
+        if (roota > 1) {
+            return -1;
+        }
+        int rootb = getRootCounts(ufb);
+        if (rootb > 1) {
+            return -1;
         }
         return res;
     }
 
-    private int find(int node, int[] pa) {
-        if (pa[node] == -1) {
-            return node;
+    private static int getRootCounts(UF ufb) {
+        int rootb = 0;
+        for (int i = 1; i < ufb.pa.length; ++i) {
+            if (ufb.pa[i] == -1) {
+                ++rootb;
+            }
         }
-        int rt = find(pa[node], pa);
-        pa[node] = rt;
-        return rt;
+        return rootb;
     }
 
     private List<Integer> edge(int[] e) {
