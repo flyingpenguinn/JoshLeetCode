58189d521b7352376ad907e264fdc520089df187
flyingpenguinn <zjm84812@gmail.com>
2026-05-20 21:17:34 -0400

    Update SpiralMatrixIII.java

--- src/SpiralMatrixIII.java
+++ src/SpiralMatrixIII.java
@@ -2,42 +2,42 @@ public class SpiralMatrixIII {
     int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     // each step we use twice: 1,1,2,2,3,3,....
+
+
     public int[][] spiralMatrixIII(int rows, int cols, int r0, int c0) {
         int[][] res = new int[rows * cols][2];
-        int i = r0;
-        int j = c0;
-        int steps = 1; // how long to walk before a turn
-        int stepsc = 2;// how many times remaining to walk the same len
-        int steped = 0; // how many steps we went from last turn
-        int dindex = 0;
-        int val = 1;
-        int rp = 0;
-        while (true) {
-            // System.out.println(i+","+j+" "+dindex+" "+steps+" "+steped);
-            if (i >= 0 && i < rows && j >= 0 && j < cols) {
-                res[rp][0] = i;
-                res[rp][1] = j;
-                rp++;
-                val++;
-                if (val == rows * cols + 1) {
+        int step = 1;
+        int cr = r0;
+        int cc = c0;
+        int cd = 0;
+        int er = r0 + 1;
+        int ec = c0 + 1;
+
+        res[0] = new int[]{cr, cc};
+        int ri = 1;
+        while (ri < res.length) {
+            int used = 0;
+            while (used < 2) {
+                int md = cd % 4;
+                int remstep = step;
+                while (remstep > 0) {
+
+                    cr += dirs[md][0];
+                    cc += dirs[md][1];
+                    if (cr >= 0 && cr < rows && cc >= 0 && cc < cols) {
+                        res[ri++] = new int[]{cr, cc};
+                    }
+                    --remstep;
+                }
+                ++cd;
+                remstep = step;
+                ++used;
+                if (cr == er && cc == ec) {
                     break;
                 }
             }
-            if (steped == steps) {
-                steped = 0;
-                dindex = (dindex + 1) % 4;
-                stepsc--;
-                if (stepsc == 0) {
-                    steps++;
-                    stepsc = 2;
-                }
-            }
-            i += dirs[dindex][0];
-            j += dirs[dindex][1];
-            steped++;
+            ++step;
         }
-
-
         return res;
     }
 
