3fe7ad024a961468a94b2ce18917406bc23d62f1
flyingpenguinn <zjm84812@gmail.com>
2026-08-02 22:04:11 -0400

    Update WidestPossibleFence.java

--- src/WidestPossibleFence.java
+++ src/WidestPossibleFence.java
@@ -1,49 +1,46 @@
-import java.util.ArrayList;
+import base.ArrayUtils;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class WidestPossibleFence {
-    public int maximumWidth(int[] planks) {
-        Map<Long, Integer> freq = new HashMap<>();
-
-        for (int v : planks) {
-            long x = v;
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
+    // value range too large? convert to freqency map and count
+    public int maximumWidth(int[] a) {
+        Map<Integer, Integer> f = new HashMap<>();
+        int n = a.length;
+        Map<Integer, Integer> rm = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            f.put(a[i], f.getOrDefault(a[i], 0) + 1);
+            rm.put(a[i], rm.getOrDefault(a[i], 0) + 1);
         }
-
-        List<Long> vals = new ArrayList<>(freq.keySet());
-        Map<Long, Integer> width = new HashMap<>();
-
-        int res = 1;
-
-        for (long x : vals) {
-            int cx = freq.get(x);
-            width.put(x, width.getOrDefault(x, 0) + cx);
-            res = Math.max(res, width.get(x));
-        }
-
-        for (int i = 0; i < vals.size(); ++i) {
-            long x = vals.get(i);
-            int cx = freq.get(x);
-
-            for (int j = i; j < vals.size(); ++j) {
-                long y = vals.get(j);
-                int cy = freq.get(y);
-
-                int add;
-                if (i == j) {
-                    add = cx / 2;
-                } else {
-                    add = Math.min(cx, cy);
+        for (int v1 : f.keySet()) {
+            int c1 = f.get(v1);
+            for (int v2 : f.keySet()) {
+                int c2 = f.get(v2);
+                if (v1 > v2) {
+                    continue;
                 }
-
-                long h = x + y;
-                width.put(h, width.getOrDefault(h, 0) + add);
-                res = Math.max(res, width.get(h));
+                int ccnt = 0;
+                int csum = 0;
+                if (v1 != v2) {
+                    ccnt = Math.min(c1, c2);
+                    csum = v1 + v2;
+                } else {
+                    ccnt = (c1) / 2;
+                    csum = v1 + v2;
+                }
+                rm.put(csum, rm.getOrDefault(csum, 0) + ccnt);
             }
         }
-
+        int res = 0;
+        for (int rk : rm.keySet()) {
+            res = Math.max(res, rm.get(rk));
+        }
         return res;
+    }
+
+    static void main() {
+        System.out.println(new WidestPossibleFence().maximumWidth(ArrayUtils.read1d("[1,3,2,5,7,5,4,2,1]")));
+        System.out.println(new WidestPossibleFence().maximumWidth(ArrayUtils.read1d("2,3,7")));
     }
 }
