977d563efbce237a15775ec7e4d1e170e7d14ca7
flyingpenguinn <zjm84812@gmail.com>
2026-09-02 20:54:22 -0400

    Update SmallestDivisibleDigitProductII.java

--- src/SmallestDivisibleDigitProductII.java
+++ src/SmallestDivisibleDigitProductII.java
@@ -1,150 +1,154 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class SmallestDivisibleDigitProductII {
-    public String smallestNumber(String num, long val) {
-        List<Integer> vec = get(val);
-        if (vec.size() == 0) return "-1";
+    // use dp to figure out length. once we can fix and reason about length everything is good
+    private int[] primes = {2, 3, 5, 7};
+    private int Max = (int) 1e9;
+    private int[] cnt2 = {0, 0, 1, 0, 2, 0, 1, 0, 3, 0};
+    private int[] cnt3 = {0, 0, 0, 1, 0, 0, 1, 0, 0, 2};
 
-        List<Integer> prefix = new ArrayList<>(List.of(0, 0, 0, 0));
-        for (int i = 0; i < num.length(); i++) {
-            List<Integer> temp = get(num.charAt(i) - '0');
-            add(prefix, temp);
-        }
-
-        int zeroIndex = -1;
-        for (int i = 0; i < num.length(); i++) {
-            if (num.charAt(i) == '0') {
-                zeroIndex = i;
-                break;
-            }
-        }
-
-        if (ok(prefix, vec) && zeroIndex == -1) {
-            return num;
-        }
-
-        int postlen = 0;
-        for (int i = num.length() - 1; i >= 0; i--) {
-            int d = num.charAt(i) - '0';
-            List<Integer> temp = get(d);
-            remove(prefix, temp);
-            if (zeroIndex != -1 && i > zeroIndex) {
-                postlen++;
+    public String smallestNumber(String num, long t) {
+        int[] cnt = new int[4];
+        for (int i = 0; i < 4; ++i) {
+            int cp = primes[i];
+            if (t % cp != 0) {
                 continue;
             }
 
-            for (int j = d + 1; j <= 9; j++) {
-                temp = get(j);
-                add(prefix, temp);
-                remove(vec, prefix); // how much needed
+            while (t % cp == 0) {
+                t /= cp;
+                ++cnt[i];
+            }
+        }
+        if (t > 1) {
+            return "-1";
+        }
 
-                List<Integer> t = construct(vec);
-                int cnt = 0;
-                for (int k = 2; k < 10; k++) cnt += t.get(k);
-
-                if (cnt <= postlen) {
-                    List<Integer> tempPrefix = add1(prefix, t);
-                    if (ok(tempPrefix, vec)) {
-                        StringBuilder ans = new StringBuilder(num.substring(0, i));
-                        ans.append((char) (j + '0'));
-                        while (cnt != postlen) { // fill empty with 1
-                            cnt++;
-                            ans.append('1');
-                        }
-                        for (int k = 1; k < 10; k++) {
-                            while (t.get(k) > 0) {
-                                ans.append((char) (k + '0'));
-                                t.set(k, t.get(k) - 1);
-                            }
-                        }
-                        return ans.toString();
-                    }
+        int[][] dp = new int[cnt[0] + 1][cnt[1] + 1];
+        for (int i = 0; i < dp.length; ++i) {
+            Arrays.fill(dp[i], Max);
+        }
+        for (int i = 0; i <= cnt[0]; ++i) {
+            for (int j = 0; j <= cnt[1]; ++j) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 0;
                 }
-
-                add(vec, prefix);
-                remove(prefix, temp);
-            }
-            postlen++;
-        }
-
-        StringBuilder ans = new StringBuilder();
-        List<Integer> t = construct(vec);
-        for (int i = 1; i < 10; i++) {
-            while (t.get(i) > 0) {
-                ans.append((char) (i + '0'));
-                t.set(i, t.get(i) - 1);
+                for (int k = 2; k <= 9; ++k) {
+                    if (k == 5 || k == 7) {
+                        continue;
+                    }
+                    int ni = Math.max(0, i - cnt2[k]);
+                    int nj = Math.max(0, j - cnt3[k]);
+                    dp[i][j] = Math.min(dp[i][j], dp[ni][nj] + 1);
+                }
             }
         }
-        StringBuilder one = new StringBuilder();
-        while (one.length() + ans.length() <= num.length()) one.append("1");
+        int[] ncnt = new int[4];
 
-        return one.append(ans).toString();
-    }
-
-    private List<Integer> get(long n) {
-        List<Integer> vec = new ArrayList<>(List.of(0, 0, 0, 0));
-        if (n == 0) return vec;
-        while (n % 2 == 0) { n /= 2; vec.set(0, vec.get(0) + 1); }
-        while (n % 3 == 0) { n /= 3; vec.set(1, vec.get(1) + 1); }
-        while (n % 5 == 0) { n /= 5; vec.set(2, vec.get(2) + 1); }
-        while (n % 7 == 0) { n /= 7; vec.set(3, vec.get(3) + 1); }
-        if (n != 1) return new ArrayList<>();
-        return vec;
-    }
-
-    private List<Integer> construct(List<Integer> vec) {
-        List<Integer> ans = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-        if (vec.get(2) > 0) ans.set(5, vec.get(2)); // for 5
-        if (vec.get(3) > 0) ans.set(7, vec.get(3)); // for 7
-        int two = Math.max(vec.get(0), 0);
-        int three = Math.max(vec.get(1), 0);
-
-        while (two >= 3) { two -= 3; ans.set(8, ans.get(8) + 1); }
-        while (three >= 2) { three -= 2; ans.set(9, ans.get(9) + 1); }
-
-        if (two == 1 && three == 0) {
-            ans.set(2, ans.get(2) + 1);
-        } else if (two == 2 && three == 1) {
-            ans.set(2, ans.get(2) + 1);
-            ans.set(6, ans.get(6) + 1);
-        } else if (two == 1 && three == 1) {
-            ans.set(6, ans.get(6) + 1);
-        } else if (two == 2 && three == 0) {
-            ans.set(4, ans.get(4) + 1);
-        } else if (two == 0 && three == 1) {
-            ans.set(3, ans.get(3) + 1);
+        int firstZero = -1;
+        for (int i = 0; i < num.length(); ++i) {
+            int cind = num.charAt(i) - '0';
+            if (cind == 0) {
+                firstZero = i;
+                break;
+            }
+            for (int j = 0; j < 4; ++j) {
+                while (cind % primes[j] == 0) {
+                    ++ncnt[j];
+                    cind /= primes[j];
+                }
+            }
         }
-        return ans;
+        if (firstZero == -1 && cover(ncnt, cnt)) {
+            return num;
+        }
+        int n = num.length();
+
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (firstZero != -1 && firstZero < i) {
+                continue;
+            }
+
+            int cind = num.charAt(i) - '0';
+            update(ncnt, cind);
+            int[] ccnt = Arrays.copyOf(cnt, 4);
+            reduce(ccnt, ncnt);
+            for (int d = cind + 1; d <= 9; ++d) {
+                int needed = getlen(ccnt, d, dp);
+                int remlen = n - 1 - i;
+                if (needed > remlen) {
+                    continue;
+                }
+                StringBuilder res = new StringBuilder(num.substring(0, i) + d);
+                update(ccnt, d);
+                build(res, remlen, ccnt, dp);
+                return res.toString();
+            }
+        }
+
+        // if here it means we cant find same len...
+        StringBuilder res = new StringBuilder();
+        int remlen = Math.max(
+                num.length() + 1,
+                dp[cnt[0]][cnt[1]] + cnt[2] + cnt[3]
+        );
+        build(res, remlen, cnt, dp);
+        return res.toString();
     }
 
-    private boolean ok(List<Integer> vec1, List<Integer> vec2) {
-        for (int i = 0; i < 4; i++) {
-            if (vec1.get(i) < vec2.get(i)) {
+    private void reduce(int[] ccnt, int[] ncnt) {
+        for (int i = 0; i < 4; ++i) {
+            ccnt[i] -= ncnt[i];
+            ccnt[i] = Math.max(0, ccnt[i]);
+        }
+    }
+
+    private void build(StringBuilder res, int remlen, int[] ccnt, int[][] dp) {
+        if (remlen == 0) {
+            return;
+        }
+        for (int d = 1; d <= 9; ++d) {
+            int crem = getlen(ccnt, d, dp);
+            if (crem > remlen - 1) {
+                continue;
+            }
+            res.append(d);
+            update(ccnt, d);
+            build(res, remlen - 1, ccnt, dp);
+            break;
+        }
+    }
+
+    private boolean cover(int[] ncnt, int[] cnt) {
+        for (int i = 0; i < 4; ++i) {
+            if (ncnt[i] < cnt[i]) {
                 return false;
             }
         }
         return true;
     }
 
-    private void add(List<Integer> vec1, List<Integer> vec2) {
-        for (int i = 0; i < 4; i++) {
-            vec1.set(i, vec1.get(i) + vec2.get(i));
+    private void update(int[] cnt, int digit) {
+        if (digit == 5) {
+            --cnt[2];
+            cnt[2] = Math.max(cnt[2], 0);
         }
+        if (digit == 7) {
+            --cnt[3];
+            cnt[3] = Math.max(cnt[3], 0);
+        }
+        int ia = cnt2[digit];
+        int ib = cnt3[digit];
+        cnt[0] -= ia;
+        cnt[0] = Math.max(cnt[0], 0);
+        cnt[1] -= ib;
+        cnt[1] = Math.max(cnt[1], 0);
     }
 
-    private void remove(List<Integer> vec1, List<Integer> vec2) {
-        for (int i = 0; i < 4; i++) {
-            vec1.set(i, vec1.get(i) - vec2.get(i));
-        }
-    }
-
-    private List<Integer> add1(List<Integer> vec1, List<Integer> vec10) {
-        List<Integer> temp = new ArrayList<>(vec1);
-        temp.set(0, temp.get(0) + vec10.get(2) + vec10.get(6) + 2 * vec10.get(4) + 3 * vec10.get(8));
-        temp.set(1, temp.get(1) + vec10.get(3) + vec10.get(6) + 2 * vec10.get(9));
-        temp.set(2, temp.get(2) + vec10.get(5));
-        temp.set(3, temp.get(3) + vec10.get(7));
-        return temp;
+    private int getlen(int[] cnt, int digit, int[][] dp) {
+        int[] ncnt = Arrays.copyOf(cnt, 4);
+        update(ncnt, digit);
+        return dp[ncnt[0]][ncnt[1]] + ncnt[2] + ncnt[3];
     }
 }
