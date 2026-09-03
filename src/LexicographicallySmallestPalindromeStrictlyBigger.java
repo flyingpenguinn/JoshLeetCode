67c05ca9fe55f6075de789f790972c6f202f7763
flyingpenguinn <zjm84812@gmail.com>
2026-09-03 19:32:13 -0400

    Update LexicographicallySmallestPalindromeStrictlyBigger.java

--- src/LexicographicallySmallestPalindromeStrictlyBigger.java
+++ src/LexicographicallySmallestPalindromeStrictlyBigger.java
@@ -1,124 +1,76 @@
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 
 class LexicographicallySmallestPalindromePermGreater {
-    public String lexPalindromicPermutation(String s, String target) {
-        char[] sc = s.toCharArray();
-        int n = sc.length;
-        int[] cnts = new int[26];
+    public String lexPalindromicPermutation(String s, String t) {
+
+        int[] cnt = new int[26];
+        int n = s.length();
+        if (n == 1) {
+            return s.compareTo(t) > 0 ? s : "";
+        }
         for (int i = 0; i < n; ++i) {
-            char c = sc[i];
-            int cind = c - 'a';
-            ++cnts[cind];
+            int cind = s.charAt(i) - 'a';
+            ++cnt[cind];
         }
-        String res = "";
-        if (n % 2 == 0) {
-            String halft = target.substring(0, n / 2);
-            String revt = halft + new StringBuilder(halft).reverse().toString();
-            if (doable(revt, cnts) && revt.compareTo(target) > 0) {
-                return revt;
-            }
-        } else {
-            String halft = target.substring(0, n / 2);
-            String revt = halft + target.charAt(n / 2) + new StringBuilder(halft).reverse().toString();
-            if (doable(revt, cnts) && revt.compareTo(target) > 0) {
-                return revt;
-            }
-        }
-        int odds = 0;
-        int oddi = -1;
+        int odd = -1;
         for (int i = 0; i < 26; ++i) {
-            if (cnts[i] % 2 == 1) {
-                ++odds;
-                oddi = i;
-            }
-            cnts[i] /= 2;
-            if (odds > 1) {
-                return "";
+            if (cnt[i] > 0 && cnt[i] % 2 == 1) {
+                if (odd != -1) {
+                    return "";
+                }
+                odd = i;
             }
         }
-
-        String mid = String.valueOf((char) ('a' + oddi));
-
-        char[] tc = target.toCharArray();
-        int end = n % 2 == 1 ? n / 2 : n / 2 - 1;
-        for (int diff = 0; diff <= end; ++diff) {
-            int[] ccnt = Arrays.copyOf(cnts, 26);
+        for (int i = 0; i < 26; ++i) {
+            cnt[i] /= 2;
+        }
+        int mid = n / 2 - 1;
+        for (int i = mid; i >= 0; --i) {
             boolean bad = false;
-            StringBuilder cur = new StringBuilder();
-            for (int i = 0; i < diff; ++i) {
-                char ctc = tc[i];
-                int cindt = ctc - 'a';
-                if (ccnt[cindt] <= 0) {
+            int[] ncnt = Arrays.copyOf(cnt, 26);
+            for (int j = 0; j < i; ++j) {
+                int tind = t.charAt(j) - 'a';
+                --ncnt[tind];
+                if (ncnt[tind] < 0) {
                     bad = true;
                     break;
                 }
-                --ccnt[cindt];
-                cur.append(ctc);
             }
             if (bad) {
                 continue;
             }
+            int tind = t.charAt(i) - 'a';
+            for (int j = tind; j < 26; ++j) {
 
-            char ctc = tc[diff];
-            int cindt = ctc - 'a';
-            if (n % 2 == 1 && diff == n / 2) {
-                if (ctc - 'a' >= oddi) {
-                    continue;
-                }
-            } else {
-                boolean found = false;
-                for (int j = cindt + 1; j < 26; ++j) {
-                    if (ccnt[j] > 0) {
-                        found = true;
-                        --ccnt[j];
-                        cur.append((char) ('a' + j));
-                        break;
+                if (ncnt[j] > 0) {
+                    // this should be at most twice.
+                    int[] nncnt = Arrays.copyOf(ncnt, 26);
+                    StringBuilder later = new StringBuilder();
+                    later.append((char) ('a' + j));
+                    --nncnt[j];
+                    for (int k = i + 1; k <= mid; ++k) {
+                        for (int p = 0; p < 26; ++p) {
+                            if (nncnt[p] > 0) {
+                                later.append((char) ('a' + p));
+                                --nncnt[p];
+                                break;
+                            }
+                        }
+                    }
+                    String firsthalf = t.substring(0, i) + later.toString();
+                    String revfirst = new StringBuilder(firsthalf).reverse().toString();
+                    String cur = "";
+                    if (odd != -1) {
+                        cur = firsthalf + (char) ('a' + odd) + revfirst;
+                    } else {
+                        cur = firsthalf + revfirst;
+                    }
+                    if (cur.compareTo(t) > 0) {
+                        return cur;
                     }
                 }
-                if (!found) {
-                    continue;
-                }
-
-
-                Deque<Integer> rem = new ArrayDeque<>();
-                for (int j = 0; j < 26; ++j) {
-                    int times = ccnt[j];
-                    for (int k = 0; k < times; ++k) {
-                        rem.add(j);
-                    }
-                }
-                for (int i = diff + 1; i < n / 2; ++i) {
-                    int cc = rem.pollFirst();
-                    --ccnt[cc];
-                    cur.append((char) (cc + 'a'));
-                }
-            }
-            StringBuilder rev = new StringBuilder(cur).reverse();
-            String cres = null;
-            if (n % 2 == 1) {
-                cres = cur.toString() + mid + rev.toString();
-            } else {
-                cres = cur.toString() + rev.toString();
-            }
-            if (res.isEmpty() || res.compareTo(cres) > 0) {
-                res = cres;
             }
         }
-        return res;
-    }
-
-    private boolean doable(String revt, int[] cnts) {
-        int[] realcnt = new int[26];
-        for (char c : revt.toCharArray()) {
-            ++realcnt[c - 'a'];
-        }
-        for (int i = 0; i < 26; ++i) {
-            if (realcnt[i] != cnts[i]) {
-                return false;
-            }
-        }
-        return true;
+        return "";
     }
 }
