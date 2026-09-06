5824ea1b08cac2c808d89b153b49621467c32a8b
flyingpenguinn <zjm84812@gmail.com>
2026-09-06 00:28:08 -0400

    111

--- src/contest/Solution2.java
+++ src/contest/Solution2.java
@@ -3,11 +3,11 @@ package contest;
 import java.util.Map;
 
 public class Solution2 {
-
+    private int MaxInt = (int) 1e9;
     private long Max = (long) 1e18;
     private long Min = -Max;
-    private long Mod = (long) (1e9+7);
-    private int[][] dirs = {{-1,0}, {1,0}, {0, -1}, {0,1}};
+    private long Mod = (long) (1e9 + 7);
+    private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     private void update(Map<Integer, Integer> m, int k, int d) {
         int nv = m.getOrDefault(k, 0) + d;
