6b91800498931d465af0b3d40a67e2b010bbe29f
flyingpenguinn <zjm84812@gmail.com>
2026-09-13 20:36:21 -0400

    Update RectangleOverlap.java

--- src/RectangleOverlap.java
+++ src/RectangleOverlap.java
@@ -1,17 +1,8 @@
 public class RectangleOverlap {
 
-    // bottomleft and ropright can be got from min and max of bl and tr
-    // then check if area is positive
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        // check null and verify
-        int[] topRight = new int[]{Math.min(rec1[2], rec2[2]), Math.min(rec1[3], rec2[3])};
-        int[] bottomLeft = new int[]{Math.max(rec1[0], rec2[0]), Math.max(rec1[1], rec2[1])};
-        if (bottomLeft[0] >= topRight[0] || bottomLeft[1] >= topRight[1]) {
-            return false;
-        } else {
-            return true;
-        }
-
+    // check if they are detached on the 4 directions!
+    public boolean isRectangleOverlap(int[] a, int[] b) {
+        return !(a[2] <= b[0] || a[0] >= b[2] || a[1] >= b[3] || a[3] <= b[1]);
     }
 
 
