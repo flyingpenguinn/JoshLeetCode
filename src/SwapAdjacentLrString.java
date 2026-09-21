ffb27d79ba029e7e920ebe49b778d07d23af25dd
flyingpenguinn <zjm84812@gmail.com>
2026-09-21 08:42:56 -0400

    Update SwapAdjacentLrString.java

--- src/SwapAdjacentLrString.java
+++ src/SwapAdjacentLrString.java
@@ -1,49 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SwapAdjacentLrString {
     // L in t are left to L in s, R are right to s. L/R splits the streak of each other
     public boolean canTransform(String s, String t) {
-        // do l from the left and r from the right
         int n = s.length();
-
-        int sl = 0;
-        int tl = 0;
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'R') {
-                sl = 0;
-            } else if (s.charAt(i) == 'L') {
-                sl++;
-            }
-            if (t.charAt(i) == 'R') {
-                tl = 0;
-            } else if (t.charAt(i) == 'L') {
-                tl++;
-            }
-            if (sl > tl) {
-                return false;
-            }
-        }
-        if (sl != tl) {
+        String ns = s.replaceAll("X", "");
+        String nr = t.replaceAll("X", "");
+        if(!ns.equals(nr)){
             return false;
         }
-
-        int sr = 0;
-        int tr = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) == 'L') {
-                sr=0;
-            } else if (s.charAt(i) == 'R') {
-                sr++;
-            }
-            if (t.charAt(i) == 'L') {
-                tr=0;
-            } else if (t.charAt(i) == 'R') {
-                tr++;
-            }
-            if (tr < sr) {
-                return false;
+        List<Integer> ls = new ArrayList<>();
+        List<Integer> rs = new ArrayList<>();
+        for(int i=0; i<n; ++i){
+            if(s.charAt(i)=='L'){
+                ls.add(i);
+            }else if(s.charAt(i)=='R'){
+                rs.add(i);
             }
         }
-        if (sr != tr) {
-            return false;
+        int li = 0;
+        int ri = 0;
+        for(int i=0; i<n; ++i){
+            if(t.charAt(i)=='L'){
+                int spos = ls.get(li++);
+                if(i>spos){
+                    return false;
+                }
+            }else if(t.charAt(i)=='R'){
+                int spos = rs.get(ri++);
+                if(i<spos){
+                    return false;
+                }
+            }
         }
         return true;
     }
