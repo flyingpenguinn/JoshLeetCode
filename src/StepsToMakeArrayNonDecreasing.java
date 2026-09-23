f0e27c7dc1d99122105c0b81960fb7496e0f23be
flyingpenguinn <zjm84812@gmail.com>
2026-09-22 23:56:39 -0400

    Update StepsToMakeArrayNonDecreasing.java

--- src/StepsToMakeArrayNonDecreasing.java
+++ src/StepsToMakeArrayNonDecreasing.java
@@ -2,20 +2,20 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class StepsToMakeArrayNonDecreasing {
+    // each later number needs round. for the next one, it needs round+1, and the intrinsic steps of that number
     public int totalSteps(int[] a) {
         int n = a.length;
-        Deque<Integer> st = new ArrayDeque<>();
-        int[] dp = new int[n];
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.isEmpty() && a[st.peek()] < a[i]) {
-                dp[i] = Math.max(dp[i] + 1, dp[st.peek()]);
-                st.pop();
-            }
-            st.push(i);
-        }
+        Deque<int[]> st = new ArrayDeque<>();
         int res = 0;
-        for (int i = 0; i < n; ++i) {
-            res = Math.max(res, dp[i]);
+        for (int i = n - 1; i >= 0; --i) {
+            int v = a[i];
+            int round = 0;
+            while (!st.isEmpty() && st.peek()[0] < v) {
+                round = Math.max(round + 1, st.pop()[1]);
+            }
+            res = Math.max(res, round);
+            //System.out.println(v+", round="+round);
+            st.push(new int[]{v, round});
         }
         return res;
     }
